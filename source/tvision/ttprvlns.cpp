513fbddf10b0aa2fa39aab08d34298a03bbb2cec
magiblot <magiblot@hotmail.com>
2019-09-14 19:18:36 +0200

    Translate TTerminal::prevLines from assembly
    And update Borland Makefile to prevent file conflicts.

--- source/tvision/ttprvlns.cpp
+++ source/tvision/ttprvlns.cpp
@@ -15,13 +15,25 @@
 #define Uses_TTerminal
 #include <tvision/tv.h>
 
-#ifndef __BORLANDC__
-
-#include <assert.h>
-
 ushort TTerminal::prevLines( ushort pos, ushort lines )
 {
-    BREAK;
+    ushort ecx;
+    if (lines != 0)
+    {
+        if (pos == queBack)
+            return queBack;
+        pos = (pos > 0 ? pos : bufSize) - 1;
+        while (lines > 0)
+        {
+            ecx = (pos > queBack ? pos - queBack : pos) + 1;
+            while (ecx-- && buffer[pos--] != '\n');
+            if (buffer[ushort(pos + 1)] == '\n')
+                --lines;
+            else if (ushort(pos + 1) == queBack)
+                return queBack;
+            else pos = bufSize - 1;
+        };
+    }
+    pos = (pos + 2 < bufSize) ? pos + 2 : 0;
+    return pos;
 }
-
-#endif
