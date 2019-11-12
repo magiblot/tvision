4b0721136ceb0a837aa3d487b89f3253edf1d2a8
magiblot <magiblot@hotmail.com>
2019-11-12 23:30:24 +0100

    Refactor tgrmv.cpp
    What was the point of having this function written in assembly?

--- source/tvision/tgrmv.cpp
+++ source/tvision/tgrmv.cpp
@@ -18,32 +18,18 @@
 
 void TGroup::removeView( TView *p )
 {
-    TView *b, *s;
-    if (last == 0)
-        goto L7;
-    s = last;
-L1:
-    b = s->next;
-    goto L5;
-L3:
-    s = b;
-    goto L1;
-L4:
-    if (last != b)
-        goto L3;
-    goto L7;
-L5:
-    if (p != b)
-        goto L4;
-    s->next = p->next;
-    if (p != last)
-        goto L7;
-    if (p != p->next)
-        goto L6;
-    s = 0;
-L6:
-    last = s;
-L7:
-    ;
+    if (last)
+    {
+        TView *s = last;
+        while (s->next != p)
+        {
+            if (s->next == last)
+                return;
+            s = s->next;
+        }
+        s->next = p->next;
+        if (p == last)
+            last = (p == p->next) ? 0 : s;
+    }
 }
 
