d3eaeb5aec698c1ab525bb22caa86d919014b8a2
magiblot <magiblot@hotmail.com>
2019-11-12 23:21:36 +0100

    Initial tvwrite.cpp refactoring
    Which discards support for 16-bit builds and removes debug hints.

--- source/tvision/tvwrite.cpp
+++ source/tvision/tvwrite.cpp
@@ -19,157 +19,119 @@
 #define Uses_TEventQueue
 #include <tvision/tv.h>
 
+#ifdef __FLAT__
+
 #include <string.h>
-
-#ifdef DEBUG_TVWRITE
-
-#include <internal/debugAsm.h>
-#define Call(a,b) { pushTrace(a); b; }
-
-#else
-
-#define Call(a,b) { b; }
-
-#endif
-
-#define Jmp(a,b) { Call(a,b); return; }
-#define Ret(a) { Jmp(a,) }
 
 extern TPoint shadowSize;
 extern uchar shadowAttr;
 
 static short X, Y, Count, wOffset;
 static const void _FAR *Buffer;
-static TView *Self, *Target;
+static TView *Target;
 
 static int edx, esi;
 
-static void L0();
 static void L10( TView * );
 static void L20( TView * );
 static void L30( TView * );
 static void L40( TView * );
 static void L50( TGroup * );
-static void L60( const ushort *, ushort * );
 
 void TView::writeView( short x, short y, short count, const void _FAR* b )
 {
-#ifdef DEBUG_TVWRITE
-    startTrace();
-#endif
-    Self = this;
     X = x; Y = y; Count = count; Buffer = b;
-    L0();
-#ifdef DEBUG_TVWRITE
-    endTrace();
-    cerr << "X: " << X << ", Y: " << Y << ", count: " << Count << endl;
-#endif
-}
-
-void L0()
-{
     wOffset = X;
     Count += X;
     edx = 0;
-    if (Y < 0)
-        Ret("L0.1");
-    if (Y >= Self->size.y)
-        Ret("L0.2");
-    if (X >= 0)
-        Jmp("L0.3", goto L1);
-    X = 0;
-L1:
-    if (Count <= Self->size.x)
-        Jmp("L1.1", goto L2);
-    Count = Self->size.x;
-L2:
-    if (X < Count)
-        Jmp("L2.1", L10(Self));
-    Ret("L2.2");
+    if (0 <= Y && Y < size.y)
+    {
+        if (X < 0)
+            X = 0;
+        if (Count > size.x)
+            Count = size.x;
+        if (X < Count)
+            L10(this);
+    }
 }
 
 void L10( TView *dest )
 {
     TGroup *owner = dest->owner;
-    if (!(dest->state & sfVisible) || owner == 0)
-        Ret("L10.1");
-    Target = dest;
-    Y += dest->origin.y;
-    X += dest->origin.x;
-    Count += dest->origin.x;
-    wOffset += dest->origin.x;
-    if (Y < owner->clip.a.y || Y >= owner->clip.b.y)
-        Ret("L10.2");
-    if (X >= owner->clip.a.x)
-        Jmp("L10.3", goto L11);
-    X = owner->clip.a.x;
-L11:
-    if (Count <= owner->clip.b.x)
-        Jmp("L11.1", goto L12);
-    Count = owner->clip.b.x;
-L12:
-    if (X >= Count)
-        Ret("L12.1");
-    Jmp("L12.2", L20(owner->last));
+    if ((dest->state & sfVisible) && owner)
+    {
+        Target = dest;
+        Y += dest->origin.y;
+        X += dest->origin.x;
+        Count += dest->origin.x;
+        wOffset += dest->origin.x;
+        if (owner->clip.a.y <= Y && Y < owner->clip.b.y)
+        {
+            if (X < owner->clip.a.x)
+                X = owner->clip.a.x;
+            if (Count > owner->clip.b.x)
+                Count = owner->clip.b.x;
+            if (X < Count)
+                L20(owner->last);
+        }
+    }
 }
 
 void L20( TView *dest )
 {
     TView *next = dest->next;
     if (next == Target)
-        Jmp("L20.1", L40(next));
-L21:
-    if (!(next->state & sfVisible))
-        Jmp("L21.1", L20(next));
-    esi = next->origin.y;
-    if (Y < esi)
-        Jmp("L21.2", L20(next));
-    esi += next->size.y;
-    if (Y < esi)
-        Jmp("L21.3", goto L23);
-    if (!(next->state & sfShadow))
-        Jmp("L21.4", L20(next));
-    esi += shadowSize.y;
-    if (Y >= esi)
-        Jmp("L21.5", L20(next));
-    esi = next->origin.x + shadowSize.x;
-    if (X >= esi)
-        Jmp("L21.6", goto L22);
-    if (Count <= esi)
-        Jmp("L21.7", L20(next));
-    Call("L21.8", L30(next));
-L22:
-    esi += next->size.x;
-    Jmp("L22.1", goto L26);
-L23:
-    esi = next->origin.x;
-    if (X >= esi)
-        Jmp("L23.1", goto L24);
-    if (Count <= esi)
-        Jmp("L23.2", L20(next));
-    Call("L23.3", L30(next));
-L24:
-    esi += next->size.x;
-    if (X >= esi)
-        Jmp("L24.1", goto L25);
-    if (Count <= esi)
-        Ret("L24.2");
-    X = esi;
-L25:
-    if (!(next->state & sfShadow))
-        Jmp("L25.1", L20(next));
-    if (Y < next->origin.y + shadowSize.y)
-        Jmp("L25.2", L20(next));
-    esi += shadowSize.x;
-L26:
-    if (X >= esi)
-        Jmp("L26.1", L20(next));
-    edx++;
-    if (Count <= esi)
-        Jmp("L26.2", L20(next));
-    Call("L26.3", L30(next));
-    edx--;
-    Jmp("L26.4", L20(next));
+        L40(next);
+    else
+    {
+        if ((next->state & sfVisible) && next->origin.y <= Y)
+        {
+            esi = next->origin.y + next->size.y;
+            if (Y < esi)
+            {
+                esi = next->origin.x;
+                if (X < esi)
+                {
+                    if (Count > esi)
+                        L30(next);
+                    else goto L20next;
+                }
+                esi += next->size.x;
+                if (X < esi)
+                {
+                    if (Count > esi)
+                        X = esi;
+                    else return;
+                }
+                if ((next->state & sfShadow) && next->origin.y + shadowSize.y <= Y)
+                    esi += shadowSize.x;
+                else goto L20next;
+            }
+            else if ((next->state & sfShadow) && Y < esi + shadowSize.y)
+            {
+                esi = next->origin.x + shadowSize.x;
+                if (X < esi)
+                {
+                    if (Count > esi)
+                        L30(next);
+                    else goto L20next;
+                }
+                esi += next->size.x;
+            }
+            else goto L20next;
+            if (X < esi)
+            {
+                edx++;
+                if (Count > esi)
+                {
+                    L30(next);
+                    edx--;
+                }
+            }
+        }
+L20next:
+        L20(next);
+    }
 }
 
 void L30( TView *dest )
@@ -178,160 +140,72 @@ void L30( TView *dest )
     int _wOffset = wOffset, _esi = esi, _edx = edx,
         _count = Count, _y = Y;
     Count = esi;
-    Call("L30.1", L20(dest));
+
+    L20(dest);
+
     Y = _y; Count = _count; edx = _edx; esi = _esi;
     wOffset = _wOffset; Target = _Target;
     X = esi;
-    Ret("L30.2");
 }
 
 void L40( TView *dest )
 {
     TGroup *owner = dest->owner;
-    if (owner->buffer == 0)
-        Jmp("L40.1", goto L44);
-    if (owner->buffer == TScreen::screenBuffer)
-        Jmp("L40.2", goto L41);
-    Call("L40.3", L50(owner));
-    Jmp("L40.4", goto L44);
-L41:
-#ifndef __FLAT__
-I   CLI
-    if (Y != TEventQueue::curMouse.where.y)
-        Jmp("L41.1", goto L42);
-    if ((unsigned int) X > (unsigned int) TEventQueue::curMouse.where.x)
-        Jmp("L41.2", goto L42);
-    if ((unsigned int) Count > (unsigned int) TEventQueue::curMouse.where.x)
-        Jmp("L41.2", goto L43);
-L42:
-    TEventQueue::mouseIntFlag = False;
-I   STI
-    Call("L42.1", L50(owner));
-    if (!TEventQueue::mouseIntFlag)
-        Jmp("L42.2", goto L44);
-L43:
-I   STI
-#endif
-    Call("L41.1", THWMouse::hide());
-    Call("L41.2", L50(owner));
-    Call("L41.3", THWMouse::show());
-L44:
-    if (owner->lockFlag != 0)
-        Ret("L44.1");
-    Jmp("L44.2", L10(owner));
+    if (owner->buffer)
+    {
+        if (owner->buffer != TScreen::screenBuffer)
+            L50(owner);
+        else
+        {
+            THWMouse::hide();
+            L50(owner);
+            THWMouse::show();
+        }
+    }
+    if (owner->lockFlag == 0)
+        L10(owner);
 }
+
+#define loByte(w)    (((uchar *)&w)[0])
+#define hiByte(w)    (((uchar *)&w)[1])
 
 void L50( TGroup *owner )
 {
     int i;
-#ifndef __FLAT__
-    ushort *dst = &owner->buffer[Y*owner->size.x + X];
-    ushort al = 0;
-    if (owner->buffer != TScreen::screenBuffer)
-        Jmp("L50.1", goto L51);
-    al = TScreen::checkSnow;
-L51:
-    const ushort *src = &((const ushort *) Buffer)[X - wOffset];
-    if (al != 0)
-        Jmp("L51.1", L60(src, dst));
-    if (edx != 0)
-        Jmp("L51.2", goto L52);
-    for (i = 0; i < Count - X; ++i)
-    {
-        dst[i] = src[i];
-    }
-    Ret("L51.3");
-L52:
-    for (i = 0; i < Count - X; ++i)
-    {
-        dst[i] = (shadowAttr << 8) | ((const uchar *)src)[2*i];
-    }
-    Ret("L52.1");
-#else
     ushort *dst = &owner->buffer[Y*owner->size.x + X];
     const ushort *src = &((const ushort *) Buffer)[X - wOffset];
     if (owner->buffer != TScreen::screenBuffer)
-        Jmp("L50.1", L60(src, dst));
-    if (edx != 0)
-        Jmp("L50.2", goto L52);
-L51:
-// Expand character/attribute pair
-    for (i = 0; i < 2*(Count - X); ++i)
     {
-        dst[i] = ((const uchar *)src)[i];
+        if (edx == 0)
+            memmove(dst, src, 2*(Count - X));
+        else
+        {
+            ushort ColorChar;
+            hiByte(ColorChar) = shadowAttr;
+            for (i = 0; i < Count - X; ++i)
+            {
+                loByte(ColorChar) = src[i];
+                dst[i] = ColorChar;
+            }
+        }
     }
-    Jmp("L51.1", goto L54);
-L52:
-// Mix in shadow attribute
-    for (i = 0; i < 2*(Count - X); i += 2)
+    else
     {
-        dst[i] = ((const uchar *)src)[i];
-        dst[i + 1] = shadowAttr;
+        if (edx == 0)
+            // Expand character/attribute pair
+            for (i = 0; i < 2*(Count - X); ++i)
+            {
+                dst[i] = ((const uchar *) src)[i];
+            }
+        else
+            // Mix in shadow attribute
+            for (i = 0; i < 2*(Count - X); i += 2)
+            {
+                dst[i] = ((const uchar *) src)[i];
+                dst[i + 1] = shadowAttr;
+            }
+        THardwareInfo::screenWrite(X, Y, dst, Count - X);
     }
-L54:
-    Call("L54.1", THardwareInfo::screenWrite(X, Y, dst, Count - X));
-    Ret("L54.2");
-#endif
-}
-
-void L60(const ushort *src, ushort *dst)
-{
-    int i;
-#ifndef __FLAT__
-    if (edx != 0)
-        Jmp("L60.1", goto L65);
-L61:
-    for (i = 0; i < Count - X; ++i)
-    {
-        ushort load = src[i];
-I       MOV     DX, 0x03DA
-L62:
-I       IN      AL, DX
-I       TEST    AL, 1
-I       JNE     L62
-I       CLI
-L63:
-I       IN      AL, DX
-I       TEST    AL, 1
-I       JE      L63
-        dst[i] = load;
-I       STI
-    }
-    Ret("L63.1");
-L65:
-    for (i = 0; i < Count - X; ++i)
-    {
-        uchar load = ((const uchar *)src)[2*i];
-I       MOV     DX, 0x03DA
-L66:
-I       IN      AL, DX
-I       TEST    AL, 1
-I       JNE     L66
-I       CLI
-L67:
-I       IN      AL, DX
-I       TEST    AL, 1
-I       JE      L67
-        dst[i] = (shadowAttr << 8) | load;
-I       STI
-    }
-    Ret("L67.1");
-#else
-    if (edx != 0)
-        Jmp("L60.1", goto L61);
-    for (i = 0; i < Count - X; ++i)
-    {
-        dst[i] = src[i];
-    }
-    edx = 0;
-    Ret("L60.2");
-L61:
-    for (i = 0; i < Count - X; ++i)
-    {
-        dst[i] = (shadowAttr << 8) | ((const uchar *)src)[2*i];
-    }
-    Ret("L61.1");
-#endif
 }
 
 void TView::writeBuf( short x, short y, short w, short h, const void _FAR* b )
@@ -371,7 +245,7 @@ void TView::writeStr( short x, short y, const char *str, uchar color )
 {
     if (str != 0)
     {
-        size_t length = strlen(str);
+        int length = strlen(str);
         if (length > 0)
         {
             ushort *attributed = new ushort[length];
@@ -386,3 +260,5 @@ void TView::writeStr( short x, short y, const char *str, uchar color )
         }
     }
 }
+
+#endif
