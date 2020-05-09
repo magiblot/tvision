07e2e5112996771389a9ded743eafe2902a81a78
magiblot <magiblot@hotmail.com>
2020-05-09 21:07:32 +0200

    Make TTerminal::prevLines easier to understand

--- source/tvision/ttprvlns.cpp
+++ source/tvision/ttprvlns.cpp
@@ -15,27 +15,37 @@
 #define Uses_TTerminal
 #include <tvision/tv.h>
 
+static Boolean backwardsFindLF( const char *buffer, ushort &p, ushort count )
+{
+    ushort pos = p;
+    while (count--)
+        if (buffer[pos--] == '\n')
+            return p = pos, True;
+    return p = pos, False;
+}
+
 ushort TTerminal::prevLines( ushort pos, ushort lines )
 {
     if (lines != 0)
     {
         if (pos == queBack)
             return queBack;
-        pos = (pos > 0 ? pos : bufSize) - 1;
+        bufDec(pos);
         while (lines > 0)
         {
+            // TTerminal uses a circular buffer. 'count' here measures how
+            // many bytes can be read before reaching the 'queue back'
+            // or the beginning of the buffer.
             ushort count = (pos > queBack ? pos - queBack : pos) + 1;
-            char c = '\0';
-            while (count-- && (c = buffer[pos--]) != '\n');
-            if (c == '\n')
+            if (backwardsFindLF(buffer, pos, count))
                 --lines;
-            else if (ushort(pos + 1) == queBack)
+            else if (ushort(pos + 1) == queBack) // 'queue back' reached.
                 return queBack;
-            else pos = bufSize - 1;
+            else
+                pos = bufSize - 1;
         };
     }
-    pos += 2;
-    if (pos >= bufSize)
-        pos -= bufSize;
+    bufInc(pos);
+    bufInc(pos);
     return pos;
 }
