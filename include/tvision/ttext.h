magiblot/tvision

commit 613265bf8ff265b5cbe0f2336c95c652b782bea0

TText::eat: fix zero-width characters not being added to last cell
The solution to this is to return a boolean from TText::eat that indicates whether it should be invoked again or not. This makes it simpler to use in some cases and more complex in others.

Related: #26.

include/tvision/ttext.h
@@ -38,7 +38,7 @@ public:
     static size_t fill(TSpan<TScreenCell> cells, TStringView text, Attr &&attr);
 #endif
 
-    static void eat(TSpan<TScreenCell> cells, size_t &width, TStringView text, size_t &bytes);
+    static Boolean eat(TSpan<TScreenCell> cells, size_t &width, TStringView text, size_t &bytes);
     static void next(TStringView text, size_t &bytes, size_t &width);
     static void wseek(TStringView text, size_t &index, size_t &remainder, int count);
 
@@ -72,14 +72,16 @@ inline size_t TText::wseek(TStringView text, int count, Boolean)
     return count > 0 ? min(count, text.size()) : 0;
 }
 
-inline void TText::eat( TSpan<TScreenCell> cells, size_t &i,
-                        TStringView text, size_t &j )
+inline Boolean TText::eat( TSpan<TScreenCell> cells, size_t &i,
+                           TStringView text, size_t &j )
 {
     if (i < cells.size() && j < text.size()) {
         ::setChar(cells[i], text[j]);
         ++i;
         ++j;
+        return True;
     }
+    return False;
 }
 
 #pragma warn -inl
@@ -171,11 +173,8 @@ inline size_t TText::fill(TSpan<TScreenCell> cells, TStringView text)
 // Note that one cell is always one column wide.
 {
     size_t w = 0, b = 0;
-    while (w < cells.size() && b < text.size())
-        TText::eat(cells, w, text, b);
-    // TText::eat always increases 'w' by the width of the processed
-    // text, but it never fills more cells than there are available.
-    return std::min<size_t>(w, cells.size());
+    while (TText::eat(cells, w, text, b));
+    return w;
 }
 
 template<class Attr>
@@ -192,15 +191,17 @@ inline size_t TText::fill(TSpan<TScreenCell> cells, TStringView text, Attr &&att
 //
 // * Otherwise, 'attr' is directly assigned to the cell's attributes.
 {
-    size_t w = 0, b = 0;
-    while (w < cells.size() && b < text.size()) {
-        if constexpr (std::is_invocable<Attr, TScreenCell&>())
-            attr(cells[w]);
-        else
-            ::setAttr(cells[w], attr);
-        TText::eat(cells, w, text, b);
+    if (cells.size() && text.size()) {
+        size_t w = 0, b = 0;
+        do {
+            if constexpr (std::is_invocable<Attr, TScreenCell&>())
+                attr(cells[w]);
+            else
+                ::setAttr(cells[w], attr);
+        } while (TText::eat(cells, w, text, b));
+        return w;
     }
-    return std::min<size_t>(w, cells.size());
+    return 0;
 }
 
 inline bool TText::isBlacklisted(TStringView mbc)
@@ -211,8 +212,8 @@ inline bool TText::isBlacklisted(TStringView mbc)
     return mbc == "\xE2\x80\x8D"; // U+200D ZERO WIDTH JOINER.
 }
 
-inline void TText::eat( TSpan<TScreenCell> cells, size_t &i,
-                        TStringView text, size_t &j )
+inline Boolean TText::eat( TSpan<TScreenCell> cells, size_t &i,
+                           TStringView text, size_t &j )
 // Reads a single character from a multibyte-encoded string, and writes it into
 // a screen cell.
 //
@@ -228,22 +229,31 @@ inline void TText::eat( TSpan<TScreenCell> cells, size_t &i,
 // combining characters appended to it (of width 0).
 //
 // So, when a zero-width character is found in 'text', it is combined with the
-// previous cell, i.e. cells[i - 1], provided that i > 0.
+// previous cell, i.e. cells[i - 1], as long as i > 0.
+//
+// Returns false when no more text can be written into 'cells'. In other words,
+// it is safe to use TText::eat in a loop, like this:
+//
+//      size_t i = 0, j = 0;
+//      while (TText::eat(cells, i, text, j));
 {
-    if (i < cells.size() && j < text.size()) {
+    if (j < text.size()) {
         wchar_t wc;
         std::mbstate_t state {};
         int len = std::mbrtowc(&wc, &text[j], text.size() - j, &state);
         if (len <= 1) {
-            if (len < 0)
-                ::setChar(cells[i], CpTranslator::toUtf8Int(text[j]));
-            else if (len == 0) // '\0'
-                ::setChar(cells[i], ' ');
-            else {
-                ::setChar(cells[i], {&text[j], 1});
+            if (i < cells.size()) {
+                if (len < 0)
+                    ::setChar(cells[i], CpTranslator::toUtf8Int(text[j]));
+                else if (len == 0) // '\0'
+                    ::setChar(cells[i], ' ');
+                else {
+                    ::setChar(cells[i], {&text[j], 1});
+                }
+                i += 1;
+                j += 1;
+                return true;
             }
-            i += 1;
-            j += 1;
         } else {
 #ifdef _WIN32
             int cWidth = TText::width({&text[j], (size_t) len});
@@ -251,8 +261,12 @@ inline void TText::eat( TSpan<TScreenCell> cells, size_t &i,
             int cWidth = TText::width(wc);
 #endif
             if (cWidth < 0) {
-                ::setChar(cells[i], "�");
-                i += 1;
+                if (i < cells.size()) {
+                    ::setChar(cells[i], "�");
+                    i += 1;
+                    j += len;
+                    return true;
+                }
             } else if (cWidth == 0) {
                 TStringView zwc {&text[j], (size_t) len};
                 // Append to the previous cell, if present.
@@ -261,19 +275,25 @@ inline void TText::eat( TSpan<TScreenCell> cells, size_t &i,
                     while (cells[--k].Char == TScreenCell::wideCharTrail && k > 0);
                     cells[k].Char.append(zwc);
                 }
+                j += len;
+                return true;
             } else {
-                uchar extraWidth = std::min<size_t>(cWidth - 1, 7);
-                ::setChar(cells[i], {&text[j], (size_t) len}, extraWidth);
-                // Fill trailing cells.
-                auto attr = ::getAttr(cells[i]);
-                size_t count = std::min<size_t>(extraWidth + 1, cells.size() - i);
-                for (size_t k = 1; k < count; ++k)
-                    ::setCell(cells[i + k], TScreenCell::wideCharTrail, attr);
-                i += count;
+                if (i < cells.size()) {
+                    uchar extraWidth = std::min<size_t>(cWidth - 1, 7);
+                    ::setChar(cells[i], {&text[j], (size_t) len}, extraWidth);
+                    // Fill trailing cells.
+                    auto attr = ::getAttr(cells[i]);
+                    size_t count = std::min<size_t>(extraWidth + 1, cells.size() - i);
+                    for (size_t k = 1; k < count; ++k)
+                        ::setCell(cells[i + k], TScreenCell::wideCharTrail, attr);
+                    i += count;
+                    j += len;
+                    return true;
+                }
             }
-            j += len;
         }
     }
+    return false;
 }
 
 inline void TText::next(TStringView text, size_t &index, size_t &width)

include/tvision/editors.h
@@ -279,7 +279,7 @@ public:
     Boolean encSingleByte;
     char charsBuf[4];
     void nextChar( TStringView, uint &P, uint &width );
-    void formatCell( TSpan<TScreenCell>, uint&, TStringView, uint& , TCellAttribs );
+    Boolean formatCell( TSpan<TScreenCell>, uint&, TStringView, uint& , TCellAttribs );
     TStringView bufChars( uint );
     TStringView bufPrevChars( uint );
 

source/tvision/drivers.cpp
@@ -288,7 +288,7 @@ void TDrawBuffer::moveCStr( ushort indent, TStringView str, ushort attrs )
     int toggle = 1;
     uchar curAttr = ((uchar *)&attrs)[0];
 
-    while (i < length() && j < str.size())
+    while (j < str.size())
         if (str[j] == '~')
         {
             curAttr = ((uchar *) &attrs)[toggle];
@@ -297,8 +297,10 @@ void TDrawBuffer::moveCStr( ushort indent, TStringView str, ushort attrs )
         }
         else
         {
-            ::setAttr(data[i], curAttr);
-            TText::eat(data, i, str, j);
+            if (i < length())
+                ::setAttr(data[i], curAttr);
+            if (!TText::eat(data, i, str, j))
+                break;
         }
 #endif
 }

source/tvision/edits.cpp
@@ -49,19 +49,23 @@ void TEditor::formatLine( TScreenCell *DrawBuf,
     for (int r = 0; r < 3; ++r)
     {
         Color = ranges[r].color;
-        while (P < ranges[r].end && X < Width)
+        while (P < ranges[r].end)
         {
             TStringView chars = bufChars(P);
             uchar Char = chars[0];
             if (Char == '\r' || Char == '\n')
                 goto fill;
             if (Char == '\t') {
-                do {
-                    ::setCell(Cells[X++], ' ', Color);
-                } while (X%8 != 0 && X < Width);
-                ++P;
+                if (X < Width) {
+                    do {
+                        ::setCell(Cells[X++], ' ', Color);
+                    } while (X%8 != 0 && X < Width);
+                    ++P;
+                } else
+                    break;
             } else
-                formatCell(Cells, (uint&) X, chars, P, Color);
+                if (!formatCell(Cells, (uint&) X, chars, P, Color))
+                    break;
         }
     }
 fill:

source/tvision/teditor1.cpp
@@ -279,13 +279,18 @@ void TEditor::nextChar( TStringView s, uint &p, uint &width )
         }
 }
 
-void TEditor::formatCell( TSpan<TScreenCell> cells, uint &width,
-                          TStringView text, uint &p, TCellAttribs color )
+Boolean TEditor::formatCell( TSpan<TScreenCell> cells, uint &width,
+                             TStringView text, uint &p, TCellAttribs color )
 {
-    ::setAttr(cells[width], color);
+    if (width < cells.size())
+        ::setAttr(cells[width], color);
     size_t p_ = 0, w_ = width;
-    TText::eat(cells, w_, text, p_);
-    p += p_; width = w_;
+    if (TText::eat(cells, w_, text, p_))
+        {
+        p += p_; width = w_;
+        return True;
+        }
+    return False;
 }
 
 int TEditor::charPos( uint p, uint target )

commit d67883844544f6485522d24d5f0a1b0b42e40740

Fix out-of-bounds array access in TText::fill
Regressed by 613265bf8ff265b5cbe0f2336c95c652b782bea0.

include/tvision/ttext.h
@@ -191,17 +191,16 @@ inline size_t TText::fill(TSpan<TScreenCell> cells, TStringView text, Attr &&att
 //
 // * Otherwise, 'attr' is directly assigned to the cell's attributes.
 {
-    if (cells.size() && text.size()) {
-        size_t w = 0, b = 0;
-        do {
+    size_t w = 0, b = 0;
+    do {
+        if (w < cells.size()) {
             if constexpr (std::is_invocable<Attr, TScreenCell&>())
                 attr(cells[w]);
             else
                 ::setAttr(cells[w], attr);
-        } while (TText::eat(cells, w, text, b));
-        return w;
-    }
-    return 0;
+        }
+    } while (TText::eat(cells, w, text, b));
+    return w;
 }
 
 inline bool TText::isBlacklisted(TStringView mbc)