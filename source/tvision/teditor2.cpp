magiblot/tvision

commit 3eaf301f78eef252ca5e19aafce31c5df354fdc3

TEditor: add support for smart Home key

source/tvision/teditor2.cpp
@@ -246,6 +246,15 @@ ushort TEditor::prevWord( ushort p )
     return p;
 }
 
+ushort TEditor::indentedLineStart( ushort P )
+{
+    ushort startPtr = lineStart(P);
+    ushort destPtr = startPtr;
+    char c;
+    while ( (c = bufChar(destPtr)) == ' ' || c == '\t' ) ++destPtr;
+    return destPtr == P ? startPtr : destPtr;
+}
+
 void TEditor::replace()
 {
     TReplaceDialogRec replaceRec( findStr, replaceStr, editorFlags );

include/tvision/editors.h
@@ -224,6 +224,7 @@ public:
     ushort lineEnd( ushort );
     ushort lineMove( ushort, int );
     ushort lineStart( ushort );
+    ushort indentedLineStart( ushort );
     void lock();
     void newLine();
     ushort nextChar( ushort );

source/tvision/teditor1.cpp
@@ -564,7 +564,7 @@ void TEditor::handleEvent( TEvent& event )
                             setCurPtr(nextWord(curPtr), selectMode);
                             break;
                         case cmLineStart:
-                            setCurPtr(lineStart(curPtr), selectMode);
+                            setCurPtr(autoIndent ? indentedLineStart(curPtr) : lineStart(curPtr), selectMode);
                             break;
                         case cmLineEnd:
                             setCurPtr(lineEnd(curPtr), selectMode);

commit 7ceac8c6d0a4cb13ca9ed79ccaedfc9c410b67ac

TEditor: fix out-of-bounds memory access
Regression by 3eaf301f78eef252ca5e19aafce31c5df354fdc3.

source/tvision/teditor2.cpp
@@ -308,7 +308,8 @@ uint TEditor::indentedLineStart( uint P )
     uint startPtr = lineStart(P);
     uint destPtr = startPtr;
     char c;
-    while ( (c = bufChar(destPtr)) == ' ' || c == '\t' ) ++destPtr;
+    while( destPtr < bufLen && ((c = bufChar(destPtr)) == ' ' || c == '\t') )
+        ++destPtr;
     return destPtr == P ? startPtr : destPtr;
 }
 