7615646b931be4cf51a3a30b73b84adf1877c80e
Brian Luft <brian@electroly.com>
2020-11-22 19:02:38 +0100

    Windows: disable the console's Quick Edit mode
    The mouse doesn't work unless Quick Edit is disabled, either by the user doing it themselves in the properties window, or by applying this code change.

--- source/platform/win32con.cpp
+++ source/platform/win32con.cpp
@@ -52,6 +52,8 @@ void Win32ConsoleStrategy::initConsole()
     GetConsoleMode(consoleHandle[cnInput], &consoleMode[cnInput]);
     consoleMode[cnInput] |= ENABLE_WINDOW_INPUT; // Report changes in buffer size
     consoleMode[cnInput] &= ~ENABLE_PROCESSED_INPUT; // Report CTRL+C and SHIFT+Arrow events.
+    consoleMode[cnInput] |= ENABLE_EXTENDED_FLAGS;   // Disable the Quick Edit mode,
+    consoleMode[cnInput] &= ~ENABLE_QUICK_EDIT_MODE; // which inhibits the mouse.
     SetConsoleMode(consoleHandle[cnInput], consoleMode[cnInput]);
     // Set the output mode.
     GetConsoleMode(consoleHandle[cnOutput], &consoleMode[cnOutput]);
