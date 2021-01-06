magiblot/tvision

commit df4e5e6e2af1896af2d49050c63886d84d57c27a

TApplication: provide default implementation of dosShell(), tile() and cascade()
These commonly used methods are defined in the example applications and most people will copy them from there, leading to duplication. This is especially bad in the case of dosShell(), which is platform-dependent, and was not even well implemented, because the __unix__ macro is not defined in all UNIX systems.

The Pascal version of Turbo Vision 2.0 not only defines these functions, but also 'WriteShellMsg' and 'GetTileRect' to control their behaviour, and handleEvent to respond to cmDosShell, cmCascade and cmTile. All of this has been added back to TApplication.

This may break existing applications, because many of them handle these commands manually.

source/tvision/tapplica.cpp
@@ -19,7 +19,11 @@
 #define Uses_TObject
 #define Uses_TMouse
 #define Uses_TApplication
+#define Uses_TDeskTop
 #include <tvision/tv.h>
+
+#include <iostream.h>
+#include <signal.h>
 
 TStaticInit::TStaticInit()
 {
@@ -65,4 +69,67 @@ void TApplication::resume()
         // to draw beyond TDrawBuffer's limits.
         setScreenMode( TDisplay::smChanged );
         }
+}
+
+void TApplication::cascade()
+{
+    if( deskTop )
+        deskTop->cascade( getTileRect() );
+}
+
+void TApplication::dosShell()
+{
+    suspend();
+    writeShellMsg();
+#if defined( __BORLANDC__ ) || defined( _WIN32 )
+    system( getenv( "COMSPEC" ) );
+#else
+    raise( SIGTSTP );
+#endif
+    resume();
+    redraw();
+}
+
+TRect TApplication::getTileRect()
+{
+    return deskTop->getExtent();
+}
+
+void TApplication::handleEvent(TEvent &event)
+{
+    TProgram::handleEvent(event);
+
+    if( event.what == evCommand )
+        {
+        switch (event.message.command)
+            {
+            case cmDosShell:
+                dosShell();
+                break;
+            case cmCascade:
+                cascade();
+                break;
+            case cmTile:
+                tile();
+                break;
+            default:
+                return;
+            }
+        clearEvent(event);
+        }
+}
+
+void TApplication::tile()
+{
+    if( deskTop )
+        deskTop->tile( getTileRect() );
+}
+
+void TApplication::writeShellMsg()
+{
+#if defined( __BORLANDC__ ) || defined( _WIN32 )
+    cout << "Type EXIT to return..." << endl;
+#else
+    cout << "The application has been stopped. You can return by entering 'fg'." << endl;
+#endif
 }

examples/tvdemo/tvcmds.h
@@ -21,7 +21,7 @@ const int cmAsciiCmd    = 103;
 const int cmCalcCmd     = 104;
 const int cmOpenCmd     = 105;
 const int cmChDirCmd    = 106;
-const int cmDOS_Cmd     = 107;
+// const int cmDOS_Cmd     = 107;
 const int cmMouseCmd    = 108;
 const int cmColorCmd    = 109;
 const int cmSaveCmd     = 110;

examples/tvdemo/tvdemo.h
@@ -49,9 +49,6 @@ private:
     void chBackground();              // Background pattern
     void openFile( const char *fileSpec );  // File Viewer
     void changeDir();                 // Change directory
-    void shell();                     // DOS shell
-    void tile();                      // Tile windows
-    void cascade();                   // Cascade windows
     void mouse();                     // Mouse control dialog box
     void colors();                    // Color control dialog box
     void outOfMemory();               // For validView() function

examples/tvdemo/tvdemo1.cpp
@@ -275,14 +275,3 @@ void TVDemo::storeDesktop(fpstream& s)
   deskTop->forEach(::writeView, &s);
   s << 0;
 }
-
-//
-// Tile function
-//
-
-void TVDemo::tile()
-{
-    deskTop->tile( deskTop->getExtent() );
-}
-
-

examples/tvdemo/tvdemo2.cpp
@@ -41,25 +41,6 @@
 #include <signal.h>
 
 //
-//  DOS Shell Command.
-//
-
-void TVDemo::shell()
-{
-    suspend();
-#ifndef __unix__
-    cout << "Type EXIT to return..." << endl;
-    system( getenv( "COMSPEC"));
-#else
-    cout << "The application has been stopped. You can return by entering 'fg'." << endl;
-    raise(SIGTSTP);
-#endif
-    resume();
-    redraw();
-}
-
-
-//
 // DemoApp::handleEvent()
 //  Event loop to distribute the work.
 //
@@ -106,18 +87,6 @@ void TVDemo::handleEvent(TEvent &event)
 
             case cmChDirCmd:            //  Change directory
                 changeDir();
-                break;
-
-            case cmDOS_Cmd:             //  DOS shell
-                shell();
-                break;
-
-            case cmTile:             //  Tile current file windows
-                tile();
-                break;
-
-            case cmCascade:          //  Cascade current file windows
-                cascade();
                 break;
 
             case cmMouseCmd:            //  Mouse control dialog box
@@ -231,6 +200,7 @@ void TVDemo::calculator()
     }
 }
 
+
 //
 // Event Viewer function
 //
@@ -240,15 +210,6 @@ void TVDemo::eventViewer()
     TEventViewer *viewer = (TEventViewer *) validView(TEventViewer::toggle());
     if(viewer != 0)
         deskTop->insert(viewer);
-}
-
-//
-// Cascade function
-//
-
-void TVDemo::cascade()
-{
-    deskTop->cascade( deskTop->getExtent() );
 }
 
 

examples/tvdemo/tvdemo3.cpp
@@ -194,7 +194,7 @@ TMenuBar *TVDemo::initMenuBar(TRect r)
         *new TMenuItem( "~O~pen...", cmOpenCmd, kbF3, hcFOpen, "F3" ) +
         *new TMenuItem( "~C~hange Dir...", cmChDirCmd, kbNoKey, hcFChangeDir ) +
          newLine() +
-        *new TMenuItem( "~D~OS Shell", cmDOS_Cmd, kbNoKey, hcFDosShell ) +
+        *new TMenuItem( "~D~OS Shell", cmDosShell, kbNoKey, hcFDosShell ) +
         *new TMenuItem( "E~x~it", cmQuit, kbAltX, hcFExit, "Alt-X" );
 
     TSubMenu& sub3 =

examples/tvedit/tvedit.h
@@ -47,10 +47,7 @@ private:
     void fileOpen();
     void fileNew();
     void changeDir();
-    void dosShell();
     void showClip();
-    void tile();
-    void cascade();
 };
 
 extern TEditWindow *clipWindow;

examples/tvedit/tvedit1.cpp
@@ -96,34 +96,10 @@ void TEditorApp::changeDir()
     execDialog( new TChDirDialog( cdNormal, 0 ), 0 );
 }
 
-void TEditorApp::dosShell()
-{
-    suspend();
-#ifndef __unix__
-    cout << "Type EXIT to return..." << endl;
-    system( getenv( "COMSPEC"));
-#else
-    cout << "The application has been stopped. You can return by entering 'fg'." << endl;
-    raise(SIGTSTP);
-#endif
-    resume();
-    redraw();
-}
-
 void TEditorApp::showClip()
 {
     clipWindow->select();
     clipWindow->show();
-}
-
-void TEditorApp::tile()
-{
-    deskTop->tile( deskTop->getExtent() );
-}
-
-void TEditorApp::cascade()
-{
-    deskTop->cascade( deskTop->getExtent() );
 }
 
 void TEditorApp::handleEvent( TEvent& event )
@@ -146,20 +122,8 @@ void TEditorApp::handleEvent( TEvent& event )
                 changeDir();
                 break;
 
-            case cmDosShell:
-                dosShell();
-                break;
-
             case cmShowClip:
                 showClip();
-                break;
-
-            case cmTile:
-                tile();
-                break;
-
-            case cmCascade:
-                cascade();
                 break;
 
             default:

commit bd497922e1c37c371c10f433370ed698acf5e5f2

tapplica.cpp: add missing include for 'system' and 'getenv'
Fixes compilation with 16-bit Borland C++.

Regressed by df4e5e6e2af1896af2d49050c63886d84d57c27a. It looks like nobody has built for DOS since then.

source/tvision/tapplica.cpp
@@ -23,6 +23,7 @@
 #include <tvision/tv.h>
 
 #include <iostream.h>
+#include <stdlib.h>
 #include <signal.h>
 
 TStaticInit::TStaticInit()