magiblot/tvision

commit 2fbf9dacee53625ca8840ebbf03cbd96abc4c370

Do not force inclusion of scrncell.h
This avoids the inclusion of <cstring>, <algorithm> and <type_traits>, which sum up to ~24000 lines of code.

include/tvision/scrncell.h
@@ -7,14 +7,10 @@
 /*                                                                         */
 /* ------------------------------------------------------------------------*/
 
-#ifndef TVISION_SCRNCELL_H
-#define TVISION_SCRNCELL_H
+#if defined( Uses_TScreenCell ) && !defined( __TScreenCell )
+#define __TScreenCell
 
 #ifdef __BORLANDC__
-
-typedef ushort TScreenCell;
-typedef uchar TCellAttribs;
-typedef uchar TCellChar;
 
 inline const TCellAttribs &getAttr(const TScreenCell &cell)
 {
@@ -334,4 +330,4 @@ inline void setCell(TScreenCell &cell, TCellChar ch, TCellAttribs attr, uchar ex
 
 #endif // __BORLANDC__
 
-#endif
+#endif // Uses_TScreenCell

include/pch.h
@@ -1,3 +1,4 @@
+#define Uses_TScreenCell
 #define Uses_TText
 #include <tvision/tv.h>
 #include <iostream.h>

include/tvision/internal/codepage.h
@@ -43,7 +43,7 @@ public:
         activeTable = &tables[0];
     }
 
-    static TCellChar toUtf8Int(unsigned char c)
+    static uint32_t toUtf8Int(unsigned char c)
     {
         return activeTable->toUtf8Int[c];
     }

include/tvision/internal/textattr.h
@@ -1,6 +1,7 @@
 #ifndef TEXTATTR_H
 #define TEXTATTR_H
 
+#define Uses_TScreenCell
 #include <tvision/tv.h>
 
 inline void swapRedBlue(TCellAttribs &c) {

include/tvision/ttext.h
@@ -13,7 +13,6 @@
 #include <tvision/internal/codepage.h>
 #include <algorithm>
 #include <cstring>
-#include <type_traits>
 #endif
 
 class TText {

include/tvision/ttypes.h
@@ -52,6 +52,16 @@ typedef ushort uint16_t;
 typedef ulong uint32_t;
 #endif
 
+#ifdef __BORLANDC__
+typedef ushort TScreenCell;
+typedef uchar TCellAttribs;
+typedef uchar TCellChar;
+#else
+struct TScreenCell;
+struct TCellAttribs;
+struct TCellChar;
+#endif
+
 const char EOS = '\0';
 
 enum StreamableInit { streamableInit };

include/tvision/tv.h
@@ -506,6 +506,7 @@
 #endif
 
 #if defined( Uses_THardwareInfo )
+#define Uses_TScreenCell
 #define __INC_HARDWARE_H
 #endif
 
@@ -535,6 +536,7 @@
 #endif
 
 #if defined( Uses_TDrawBuffer )
+#define Uses_TScreenCell
 #define __INC_DRAWBUF_H
 #endif
 
@@ -625,14 +627,22 @@
 #endif
 
 #if defined( Uses_TText )
+#define Uses_TScreenCell
 #define __INC_TTEXT_H
+#endif
+
+#if defined( Uses_TScreenCell )
+#define __INC_SCRNCELL_H
 #endif
 
 #include <tvision/config.h>
 #include <tvision/ttypes.h>
 #include <tvision/tspan.h>
 #include <tvision/tstrview.h>
+
+#if defined( __INC_SCRNCELL_H )
 #include <tvision/scrncell.h>
+#endif
 
 #if defined( __INC_HARDWARE_H )
 #include <tvision/hardware.h>

commit 1e8903542eb09d7a10ac313ca358ba58667cbe61

Remove unnecessary overload of TScreenCell::setChar
This would cause all integer types to cast implicitly to uchar instead of TCellChar. As a result, codepage-to-UTF-8 conversion broke in 2fbf9dacee53625ca8840ebbf03cbd96abc4c370.

include/tvision/scrncell.h
@@ -324,14 +324,6 @@ inline void setChar(TScreenCell &cell, TStringView text, uchar extraWidth=0)
     ::setChar(cell, ch, extraWidth);
 }
 
-inline void setChar(TScreenCell &cell, uchar ch)
-{
-    TScreenCell c = cell;
-    c.Char = ch;
-    c.extraWidth = 0;
-    cell = c;
-}
-
 inline void setCell(TScreenCell &cell, TCellChar ch, TCellAttribs attr, uchar extraWidth=0)
 {
     TScreenCell c {};