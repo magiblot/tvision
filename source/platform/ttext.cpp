magiblot/tvision

commit 99c9593043bf55ae699ac16225409b4a2a518a25

Improve UTF-8 decoding performance
This bypasses the locale settings, so some UTF-8 features will still work when a non-UTF-8 locale is in use.

source/platform/ttext.cpp
@@ -7,42 +7,67 @@
 #include <internal/win32con.h>
 #include <internal/winwidth.h>
 #include <wchar.h>
-#ifdef _WIN32
-#include <uchar.h>
-#endif
+
+// Copyright (c) 2008-2010 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+
+enum { UTF8_ACCEPT = 0, UTF8_REJECT = 12 };
+
+static const uint8_t utf8d[] =
+{
+    // The first part of the table maps bytes to character classes that
+    // to reduce the size of the transition table and create bitmasks.
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+     7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+     8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+    10,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3, 11,6,6,6,5,8,8,8,8,8,8,8,8,8,8,8,
+
+    // The second part is a transition table that maps a combination
+    // of a state of the automaton and a character class to a state.
+     0,12,24,36,60,96,84,12,12,12,48,72, 12,12,12,12,12,12,12,12,12,12,12,12,
+    12, 0,12,12,12,12,12, 0,12, 0,12,12, 12,24,12,12,12,12,12,24,12,24,12,12,
+    12,12,12,12,12,12,12,24,12,12,12,12, 12,24,12,12,12,12,12,12,12,24,12,12,
+    12,12,12,12,12,12,12,36,12,36,12,12, 12,36,12,12,12,12,12,36,12,36,12,12,
+    12,36,12,12,12,12,12,12,12,12,12,12,
+};
+
+static inline
+uint32_t decode_utf8(uint32_t* state, uint32_t* codep, uint8_t byte)
+{
+    uint32_t type = utf8d[byte];
+
+    *codep = (*state != UTF8_ACCEPT) ?
+        (byte & 0x3F) | (*codep << 6) :
+        (0xFF >> type) & (byte);
+
+    *state = utf8d[256 + *state + type];
+    return *state;
+}
 
 namespace ttext
 {
 
     static int mbtowc(char32_t &wc, TStringView text)
     {
-        mbstate_t state {};
-#ifdef _WIN32
-        // Windows has 16-bit wchar_t, so use char32_t.
-        int len = mbrtoc32(&wc, text.data(), text.size(), &state);
-#ifdef __MINGW32__
-        // 'mbrtowc' is buggy and always returns 1... What's worse, 'mbrtoc32' is also
-        // buggy and does not check if the first parameter is NULL and crashes.
-        // And it doesn't end there, because it always assumes the input to be
-        // valid UTF-8.
-        if (len >= 1)
+        uint32_t state = 0;
+        uint32_t codep = 0;
+        for (size_t i = 0; i < text.size(); ++i)
         {
-            for (int i = 1; i < len; ++i)
-                if ((text[i] & 0b11000000) != 0b10000000)
+            switch (decode_utf8(&state, &codep, text[i]))
+            {
+                case UTF8_ACCEPT:
+                    return (wc = codep), i + 1;
+                case UTF8_REJECT:
                     return -1;
-            if ((wc > 0x10FFFF) | ((0xD800 <= wc) & (wc <= 0xDFFF)))
-                return -1;
+                default:
+                    break;
+            }
         }
-#endif // __MINGW32__
-#else
-        static_assert(sizeof(char32_t) >= sizeof(wchar_t), "");
-        // mbrtoc32 is not available on some Unix systems (e.g. Mac OS),
-        // so we can't rely on it.
-        // Also, wcwidth is locale-dependent, so it's convenient to get the
-        // wide character from another locale-dependent function.
-        int len = mbrtowc((wchar_t *) &wc, text.data(), text.size(), &state);
-#endif // _WIN32
-        return len;
+        return -1;
     }
 
 }

COPYRIGHT
@@ -33,12 +33,12 @@ in Technical Data and Computer Software clause at DFARS
 Contractor / manufacturer is Borland International, Inc.,
 100 Borland Way, Scotts Valley, CA 95066.
 
-The copyright below applies only to the modifications made by magiblot1 to
+The copyright below applies only to the modifications made by magiblot to
 the original source code and the source code that was written from scratch.
 
 MIT License
 
-Copyright 2019-2020 magiblot <magiblot@hotmail.com>
+Copyright 2019-2021 magiblot <magiblot@hotmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -57,3 +57,62 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+Below are the copyright notices of third-party components included in this
+project.
+
+## Fast utoa()
+## https://github.com/miloyip/itoa-benchmark
+
+Copyright (C) 2014 Milo Yip
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+## wcwidth() for Unicode 5.0
+## https://www.cl.cam.ac.uk/~mgk25/ucs/wcwidth.c
+
+Markus Kuhn -- 2007-05-26 (Unicode 5.0)
+
+Permission to use, copy, modify, and distribute this software
+for any purpose and without fee is hereby granted. The author
+disclaims all warranties with regard to this software.
+
+## Flexible and Economical UTF-8 Decoder
+## http://bjoern.hoehrmann.de/utf-8/decoder/dfa/
+
+Copyright (c) 2008-2010 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.

commit dd5da212548ae0af7ac4a05456fae3d8b5211b5c

Fix handling of null characters in TText::eat
99c9593043bf55ae699ac16225409b4a2a518a25 broke the ttext::mbtowc contract.

source/platform/ttext.cpp
@@ -52,6 +52,8 @@ namespace ttext
 {
 
     static int mbtowc(char32_t &wc, TStringView text)
+    // Returns n >= 1 if 'text' begins with a UTF-8 multibyte character that's
+    // 'n' bytes long, -1 otherwise.
     {
         uint32_t state = 0;
         uint32_t codep = 0;
@@ -156,7 +158,7 @@ TText::eat_r TText::eat_internal( TSpan<TScreenCell> cells, size_t i,
                 // combining characters.
                 if (mb.length < 0)
                     ::setChar(cells[i], CpTranslator::toUtf8Int(text[j]));
-                else if (mb.length == 0) // '\0'
+                else if (text[j] == '\0')
                     ::setChar(cells[i], ' ');
                 else
                     ::setChar(cells[i], text[j]);