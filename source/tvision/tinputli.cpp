magiblot/tvision

commit de680b9939d1e315eabfd499595eedcc0c27e6dc

TInputLine: add support for specifying a maximum text width

source/tvision/tinputli.cpp
@@ -49,17 +49,20 @@ char hotKey( const char *s ) noexcept
 
 #define cpInputLine "\x13\x13\x14\x15"
 
-TInputLine::TInputLine( const TRect& bounds, uint aMaxLen, TValidator *aValid ) noexcept :
+TInputLine::TInputLine( const TRect& bounds, uint limit, TValidator *aValid, ushort limitMode ) noexcept :
     TView(bounds),
-    data( new char[aMaxLen] ),
-    maxLen( aMaxLen-1 ),
+    maxLen      ( (limitMode == ilMaxBytes) ? min(max(limit-1, 0), 255) : 255 ),
+    maxWidth    ( (limitMode == ilMaxWidth) ? limit : UINT_MAX ),
+    maxGraphemes( (limitMode == ilMaxGraphemes) ? limit : UINT_MAX ),
     curPos( 0 ),
     firstPos( 0 ),
     selStart( 0 ),
     selEnd( 0 ),
-    validator( aValid ),
-    oldData( new char[aMaxLen] )
+    validator( aValid )
 {
+    data = new char[maxLen + 1];
+    oldData = new char[maxLen + 1];
+
     state |= sfCursorVis;
     options |= ofSelectable | ofFirstClick;
     *data = EOS;
@@ -226,28 +229,25 @@ void TInputLine::restoreState()
 Boolean TInputLine::checkValid(Boolean noAutoFill)
 {
     int oldLen, newLen;
-    char *newData;
+    char newData[256];
 
     if (validator)
         {
         oldLen = strlen(data);
-        newData = new char[256];
         strcpy(newData, data);
         if (!validator->isValidInput(newData, noAutoFill))
             {
-                restoreState();
-                delete[] newData;
-                return False;
+            restoreState();
+            return False;
             }
         else
             {
             if (strlen(newData) > maxLen)
                 newData[maxLen] = 0;
-            strcpy(data,newData);
+            strcpy(data, newData);
             newLen = strlen(data);
             if ((curPos >= oldLen) && (newLen > oldLen))
                 curPos = newLen;
-            delete[] newData;
             return True;
             }
         }
@@ -368,7 +368,12 @@ void TInputLine::handleEvent( TEvent& event )
 
                             if( checkValid(True) )
                                 {
-                                if( strlen(data) + len <= maxLen )
+                                TTextMetrics dataMts = TText::measure(data);
+                                TTextMetrics keyMts = TText::measure(keyText);
+                                if( strlen(data) + len <= maxLen &&
+                                    dataMts.width + keyMts.width <= maxWidth &&
+                                    dataMts.graphemeCount + keyMts.graphemeCount <= maxGraphemes
+                                  )
                                     {
                                     if( firstPos > curPos )
                                         firstPos = curPos;

include/tvision/dialogs.h
@@ -150,6 +150,11 @@ inline opstream& operator << ( opstream& os, TDialog* cl )
 #if defined( Uses_TInputLine ) && !defined( __TInputLine )
 #define __TInputLine
 
+const ushort
+    ilMaxBytes      = 0,
+    ilMaxWidth      = 1,
+    ilMaxGraphemes  = 2;
+
 class _FAR TRect;
 struct _FAR TEvent;
 class _FAR TValidator;
@@ -159,7 +164,7 @@ class TInputLine : public TView
 
 public:
 
-    TInputLine( const TRect& bounds, uint aMaxLen, TValidator *aValid = 0 ) noexcept;
+    TInputLine( const TRect& bounds, uint limit, TValidator *aValid = 0, ushort limitMode = ilMaxBytes ) noexcept;
     ~TInputLine();
 
     virtual ushort dataSize();
@@ -175,6 +180,8 @@ public:
 
     char* data;
     uint maxLen;
+    uint maxWidth;
+    uint maxGraphemes;
     int curPos;
     int firstPos;
     int selStart;

include/tvision/ttext.h
@@ -9,12 +9,22 @@
 #if defined( Uses_TText ) && !defined( __TText )
 #define __TText
 
+struct TTextMetrics
+{
+    uint width;
+    uint characterCount;
+    uint graphemeCount; // Number of non-combining characters.
+};
+
 class TText
 {
 public:
 
     // Returns the width of 'text'.
     static size_t width(TStringView text) noexcept;
+
+    // Returns the width, the character count and the grapheme count of 'text'.
+    static TTextMetrics measure(TStringView text) noexcept;
 
     // Returns the length in bytes of the first character in 'text'.
     static size_t next(TStringView text) noexcept;
@@ -139,6 +149,16 @@ inline size_t TText::width(TStringView text)
 {
     return text.size();
 }
+
+#pragma warn -inl
+
+inline TTextMetrics TText::measure(TStringView text)
+{
+    TTextMetrics metrics = {text.size(), text.size(), text.size()};
+    return metrics;
+}
+
+#pragma warn .inl
 
 inline size_t TText::next(TStringView text)
 {

source/platform/ttext.cpp
@@ -160,6 +160,22 @@ size_t TText::width(TStringView text) noexcept
     return width;
 }
 
+TTextMetrics TText::measure(TStringView text) noexcept
+{
+    TTextMetrics metrics {};
+    size_t i = 0;
+    while (true)
+    {
+        size_t width = 0;
+        if (!TText::next(text, i, width))
+            break;
+        metrics.width += width;
+        metrics.characterCount += 1;
+        metrics.graphemeCount += (width > 0);
+    }
+    return metrics;
+}
+
 size_t TText::next(TStringView text) noexcept
 {
     if (text.size())

commit d8d1f64aa1c290111dc4a1f5f25e1faf7187f91b

TInputLine: store missing fields in streams
Regressed by de680b9939d1e315eabfd499595eedcc0c27e6dc.

source/tvision/tinputli.cpp
@@ -493,7 +493,7 @@ void TInputLine::setValidator( TValidator* aValid )
 void TInputLine::write( opstream& os )
 {
     TView::write( os );
-    os << maxLen << curPos << firstPos
+    os << maxLen << maxWidth << maxGraphemes << curPos << firstPos
        << selStart << selEnd;
     os.writeString( data);
     os << validator;
@@ -502,7 +502,7 @@ void TInputLine::write( opstream& os )
 void *TInputLine::read( ipstream& is )
 {
     TView::read( is );
-    is >> maxLen >> curPos >> firstPos
+    is >> maxLen >> maxWidth >> maxGraphemes >> curPos >> firstPos
        >> selStart >> selEnd;
     data = new char[maxLen + 1];
     oldData = new char[maxLen + 1];