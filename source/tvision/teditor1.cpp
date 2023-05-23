magiblot/tvision

commit 619eee17237f5816abf36b0bb08f5fd7c560aa27

TEditor: do not overwrite text when pasting

source/tvision/teditor1.cpp
@@ -624,18 +624,23 @@ void TEditor::handleEvent( TEvent& event )
               )
                 {
                 lock();
-                if( overwrite == True && hasSelection() == False )
-                    if( curPtr != lineEnd(curPtr) )
-                        selEnd = nextChar(curPtr);
-
-                if( encSingleByte )
-                    insertText( &event.keyDown.charScan.charCode, 1, False);
-                else
+                if( event.keyDown.controlKeyState & kbPaste )
                     {
                     char buf[512];
                     size_t length;
                     while( textEvent( event, TSpan<char>(buf, sizeof(buf)), length ) )
                         insertMultilineText( buf, (uint) length, False );
+                    }
+                else
+                    {
+                    if( overwrite == True && hasSelection() == False )
+                        if( curPtr != lineEnd(curPtr) )
+                            selEnd = nextChar(curPtr);
+
+                    if( encSingleByte )
+                        insertText( &event.keyDown.charScan.charCode, 1, False );
+                    else
+                        insertText( event.keyDown.text, event.keyDown.textLength, False );
                     }
 
                 trackCursor(centerCursor);

commit 9c9608e03b6df011b811f284b4c73459a7246c2d

TEditor: fix typing tabulators
Regressed by 619eee17237f5816abf36b0bb08f5fd7c560aa27.

source/tvision/teditor1.cpp
@@ -618,7 +618,7 @@ void TEditor::handleEvent( TEvent& event )
             break;
 
         case evKeyDown:
-            if( ( !encSingleByte && event.keyDown.textLength ) ||
+            if( ( !encSingleByte && event.keyDown.textLength > 0 ) ||
                 event.keyDown.charScan.charCode == 9 ||
                 ( event.keyDown.charScan.charCode >= 32 && event.keyDown.charScan.charCode < 255 )
               )
@@ -637,10 +637,10 @@ void TEditor::handleEvent( TEvent& event )
                         if( curPtr != lineEnd(curPtr) )
                             selEnd = nextChar(curPtr);
 
-                    if( encSingleByte )
-                        insertText( &event.keyDown.charScan.charCode, 1, False );
-                    else
+                    if( !encSingleByte && event.keyDown.textLength > 0 )
                         insertText( event.keyDown.text, event.keyDown.textLength, False );
+                    else
+                        insertText( &event.keyDown.charScan.charCode, 1, False );
                     }
 
                 trackCursor(centerCursor);