magiblot/tvision

commit 5a9d7156de70cf7a843b9db63f82b47f84bffa9e

Avoid loop when measuring a TScreenCell's text length
We overcome an unnecessary overhead by storing the text length in the unused bits.

include/tvision/scrncell.h
@@ -56,7 +56,9 @@ struct TCellChar
     enum : uint8_t { fWide = 0x1, fTrail = 0x2 };
 
     char _text[15];
-    uint8_t _flags;
+    uint8_t
+        _textLength : 4,
+        _flags : 4;
 
     TCellChar() = default;
     inline void moveChar(char ch);
@@ -78,6 +80,7 @@ inline void TCellChar::moveChar(char ch)
 {
     memset(this, 0, sizeof(*this));
     _text[0] = ch;
+    _textLength = 1;
 }
 
 inline void TCellChar::moveMultiByteChar(uint32_t mbc, bool wide)
@@ -86,6 +89,9 @@ inline void TCellChar::moveMultiByteChar(uint32_t mbc, bool wide)
     memset(this, 0, sizeof(*this));
     memcpy(_text, &mbc, sizeof(mbc));
     _flags = -int(wide) & fWide;
+    _textLength = 1 + ((mbc & 0xFF00) != 0) +
+                      ((mbc & 0xFF0000) != 0) +
+                      ((mbc & 0xFF000000) != 0);
 }
 
 inline void TCellChar::moveMultiByteChar(TStringView mbc, bool wide)
@@ -102,6 +108,7 @@ inline void TCellChar::moveMultiByteChar(TStringView mbc, bool wide)
             case 2: _text[1] = mbc[1];
             case 1: _text[0] = mbc[0];
         }
+        _textLength = mbc.size();
     }
 }
 
@@ -136,6 +143,7 @@ constexpr inline void TCellChar::appendZeroWidthChar(TStringView mbc)
             case 2: _text[sz + 1] = mbc[1];
             case 1: _text[sz] = mbc[0];
         }
+        _textLength = sz + mbc.size();
     }
 }
 
@@ -146,9 +154,9 @@ constexpr inline TStringView TCellChar::getText() const
 
 constexpr inline size_t TCellChar::size() const
 {
-    size_t i = 0;
-    while (++i < sizeof(_text) && _text[i]);
-    return i;
+    // There is always at least one character, even if it is a null byte and
+    // '_textLength' is zero (e.g. because the TCellChar was zero-initialized).
+    return max(_textLength, 1);
 }
 
 constexpr inline char& TCellChar::operator[](size_t i)

source/platform/ttext.cpp
@@ -308,10 +308,8 @@ TText::Lw TText::drawOneImpl( TSpan<TScreenCell> cells, size_t i,
             if (i < cells.size())
             {
                 // We need to convert control characters here since we
-                // might later try to append combining characters to this.
-                if (text[j] == '\0')
-                    cells[i]._ch.moveChar(' ');
-                else if (text[j] < ' ' || '\x7F' <= text[j])
+                // might later try to append combining characters to them.
+                if (text[j] < ' ' || '\x7F' <= text[j])
                     cells[i]._ch.moveMultiByteChar(CpTranslator::toPackedUtf8(text[j]));
                 else
                     cells[i]._ch.moveChar(text[j]);
@@ -392,10 +390,7 @@ TText::Lw TText::drawOneImpl( TSpan<TScreenCell> cells, size_t i,
             if (i < cells.size())
             {
                 bool wide = width > 1;
-                if (textU32[j] == '\0')
-                    cells[i]._ch.moveChar(' ');
-                else
-                    cells[i]._ch.moveMultiByteChar(textU8, wide);
+                cells[i]._ch.moveMultiByteChar(textU8, wide);
                 bool drawTrail = (wide && i + 1 < cells.size());
                 if (drawTrail)
                     cells[i + 1]._ch.moveWideCharTrail();

test/platform/ttext.test.cpp
@@ -5,6 +5,7 @@
 
 #define COMBINING_ZIGZAG_UTF8 "\xCD\x9B"
 #define COMBINING_ZIGZAG_UTF32 U"\u035B"
+#define ZERO_WIDTH_JOINER_UTF8 "\xE2\x80\x8D"
 
 static std::ostream &operator<<(std::ostream &os, TSpan<const char32_t> span)
 {
@@ -26,7 +27,7 @@ TEST(TText, ShouldConvertUtf8ControlCharacters)
 {
     static const TestCase<TStringView> testCases[] =
     {
-        {{"\0", 1}, " "},
+        {{"\0", 1}, {"\0", 1}},
         {"\x01", "☺"},
         {"\x1F", "▼"},
         {" ", " "},
@@ -52,7 +53,7 @@ TEST(TText, ShouldConvertUtf32ControlCharacters)
 {
     static const TestCase<TSpan<const char32_t>, TStringView> testCases[] =
     {
-        {{U"\0", 1}, " "},
+        {{U"\0", 1}, {"\0", 1}},
         {{U"\x01", 1}, "�"},
         {{U"\x1F", 1}, "�"},
         {{U" ", 1}, " "},
@@ -71,6 +72,34 @@ TEST(TText, ShouldConvertUtf32ControlCharacters)
         size_t i = 0, j = 0;
         while(TText::drawOne(cells, i, input, j));
         TStringView actual = cells[0]._ch.getText();
+        expectResultMatches(actual, testCase);
+    }
+}
+
+TEST(TText, ShouldDrawTextInScreenCells)
+{
+    enum { nCells = 2 };
+    static const TestCase<TStringView, std::vector<TStringView>> testCases[] =
+    {
+        {"a", {"a", {"\0", 1}}},
+        {"aa", {"a", "a"}},
+        {{"\0", 1}, {{"\0", 1}, {"\0", 1}}},
+        {{"\0\0", 2}, {{"\0", 1}, {"\0", 1}}},
+        {{"\0a", 2}, {{"\0", 1}, "a"}},
+        {"a" COMBINING_ZIGZAG_UTF8, {"a" COMBINING_ZIGZAG_UTF8, {"\0", 1}}},
+        {{"\0" COMBINING_ZIGZAG_UTF8, 3}, {" " COMBINING_ZIGZAG_UTF8, {"\0", 1}}},
+        {"क्"  ZERO_WIDTH_JOINER_UTF8 "ष", {"क्", "ष"}},
+    };
+
+    for (auto &testCase : testCases)
+    {
+        TScreenCell cells[nCells] {};
+        TText::drawStr(cells, 0, testCase.input, 0);
+
+        std::vector<TStringView> actual(nCells);
+        for (int i = 0; i < nCells; ++i)
+            actual[i] = cells[i]._ch.getText();
+
         expectResultMatches(actual, testCase);
     }
 }

commit bef194f3a11cbec5e6d8c08f280f64949d3b92c0

Fix initializing screen cell text in big endian systems
Regressed by 5a9d7156de70cf7a843b9db63f82b47f84bffa9e.

include/tvision/scrncell.h
@@ -89,9 +89,15 @@ inline void TCellChar::moveMultiByteChar(uint32_t mbc, bool wide)
     memset(this, 0, sizeof(*this));
     memcpy(_text, &mbc, sizeof(mbc));
     _flags = -int(wide) & fWide;
+#ifndef TV_BIG_ENDIAN
     _textLength = 1 + ((mbc & 0xFF00) != 0) +
                       ((mbc & 0xFF0000) != 0) +
                       ((mbc & 0xFF000000) != 0);
+#else
+    _textLength = 1 + ((mbc & 0xFF0000) != 0) +
+                      ((mbc & 0xFF00) != 0) +
+                      ((mbc & 0xFF) != 0);
+#endif
 }
 
 inline void TCellChar::moveMultiByteChar(TStringView mbc, bool wide)