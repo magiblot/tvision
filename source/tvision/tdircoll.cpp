magiblot/tvision

commit ceed515e7242bb4874831add9d2cce7efac28c9a

Add Windows support for FindFirst and dir functions

source/tvision/tdircoll.cpp
@@ -34,7 +34,7 @@
 
 Boolean driveValid( char drive )
 {
-#ifdef __BORLANDC__
+#ifdef _WIN32
 #if !defined( __FLAT__ )
 I       MOV     AH, 19H     // Save the current drive in BL
 I       INT     21H
@@ -81,10 +81,10 @@ Boolean isDir( const char *str )
 Boolean pathValid( const char *path )
 {
     char expPath[MAXPATH];
-    strcpy( expPath, path );
+    strnzcpy( expPath, path, MAXPATH );
     fexpand( expPath );
     int len = strlen(expPath);
-#ifdef __BORLANDC__
+#ifdef _WIN32
     if( len <= 3 )
         return driveValid(expPath[0]);
 #else

include/tvision/internal/findfrst.h
@@ -4,9 +4,7 @@
 #include <string>
 #include <vector>
 #include <mutex>
-#include <unistd.h>
-#include <dirent.h>
-#include <sys/stat.h>
+#include <filesystem>
 
 #include <tvision/tv.h>
 #include <dos.h>
@@ -19,8 +17,12 @@
 
 class FindFirstRec
 {
+    using directory_iterator = std::filesystem::directory_iterator;
+    using directory_entry = std::filesystem::directory_entry;
+    using path = std::filesystem::path;
+
     struct find_t     *finfo;
-    DIR               *dirStream;
+    directory_iterator dirStream;
     unsigned           searchAttr;
     std::string        searchDir;
     std::string        wildcard;
@@ -34,11 +36,12 @@ public:
 
 private:
 
+    bool streamValid();
     bool open();
     void close();
     bool setParameters(unsigned int, const char *);
     bool setPath(const char*);
-    bool matchEntry(struct dirent*);
+    bool matchEntry(const char *);
 
     bool attrMatch(unsigned int attrib);
     static bool wildcardMatch(char const *wildcard, char const *filename);

include/tvision/internal/pathconv.h
@@ -8,17 +8,17 @@
 
 // path_dos2unix: replaces '\' with '/' and removes drive letter.
 
-inline void path_dos2unix(std::string &s) {
+inline void path_dos2unix(std::string &s, bool drive=true) {
     std::replace(s.begin(), s.end(), '\\', '/');
-    if (s.size() > 1 && s[1] == ':' && isalpha(s[0]))
+    if (drive && s.size() > 1 && s[1] == ':' && isalpha(s[0]))
         s = s.substr(2);
 }
 
-inline void path_dos2unix(char *c) {
+inline void path_dos2unix(char *c, bool drive=true) {
     char *d = c;
     while ((d = strchr(d, '\\')))
         *d = '/';
-    if (*c && c[1] == ':' && isalpha(*c))
+    if (drive && *c && c[1] == ':' && isalpha(*c))
         memmove(c, c+2, strlen(c)-1); // Copies null terminator as well.
 }
 

source/linux/dir.cpp
@@ -41,7 +41,14 @@ void fnmerge( char *pathP, const char *driveP, const char *dirP,
     // UNIX-formatted path fixes most cases of filesystem access.
     memset(pathP, 0, MAXPATH);
     size_t n = 0;
-    // Drive letter ignored.
+#ifdef _WIN32
+    if (driveP && *driveP)
+    {
+        n += strnzcpy(&pathP[n], driveP, MAXPATH - n);
+        if (pathP[n-1] != ':')
+            n += strnzcpy(&pathP[n], ":", MAXPATH - n);
+    }
+#endif
     if (dirP && *dirP)
     {
         n += strnzcpy(&pathP[n], dirP, MAXPATH - n);
@@ -56,7 +63,9 @@ void fnmerge( char *pathP, const char *driveP, const char *dirP,
             n += strnzcpy(&pathP[n], ".", MAXPATH - n);
         n += strnzcpy(&pathP[n], extP, MAXPATH - n);
     }
+#ifdef _TV_UNIX
     path_dos2unix(pathP);
+#endif
 }
 
 static bool CopyComponent(char* dstP, const char* startP, const char* endP, size_t MAX)
@@ -156,11 +165,16 @@ int getcurdir(int drive, char *direc)
     // Note that drive 0 is the 'default' drive, 1 is drive A, etc.
     if (drive == 0 || drive-1 == getdisk())
     {
-        char buf[MAXDIR+1];
-        if (getcwd(buf, MAXDIR+1))
+#ifdef _WIN32
+        constexpr int lead = 3;
+#else
+        constexpr int lead = 1;
+#endif
+        char buf[MAXDIR+lead];
+        if (getcwd(buf, MAXDIR+lead))
         {
             path_unix2dos(buf);
-            strnzcpy(direc, buf+1, MAXDIR);
+            strnzcpy(direc, buf+lead, MAXDIR);
             return 0;
         }
     }

source/linux/findfrst.cpp
@@ -1,5 +1,11 @@
 #include <internal/findfrst.h>
 #include <internal/pathconv.h>
+#include <system_error>
+#include <sys/stat.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #define SPECIAL_BITS (_A_SUBDIR|_A_HIDDEN|_A_SYSTEM)
 
@@ -56,9 +62,14 @@ FindFirstRec* FindFirstRec::get(struct find_t *fileinfo)
     return 0;
 }
 
+bool FindFirstRec::streamValid()
+{
+    return dirStream != directory_iterator();
+}
+
 bool FindFirstRec::setParameters(unsigned int attrib, const char *pathname)
 {
-    if (!dirStream)
+    if (!streamValid())
     {
         searchAttr = attrib;
         if (setPath(pathname))
@@ -69,25 +80,40 @@ bool FindFirstRec::setParameters(unsigned int attrib, const char *pathname)
 
 bool FindFirstRec::next()
 {
-    struct dirent *e;
-    bool found = false;
-    while ((e = readdir(dirStream)) && !(found = matchEntry(e)));
-    if (!e)
-        close();
-    return found;
+    while (streamValid())
+    {
+        const auto &&pathName = dirStream->path().filename();
+        const auto &nameNative = pathName.native();
+        ++dirStream;
+#ifdef _WIN32
+        char fileName[sizeof(find_t::name)] = {0};
+        WideCharToMultiByte(CP_UTF8, 0,
+                            nameNative.data(), nameNative.size(),
+                            fileName, sizeof(fileName),
+                            nullptr, nullptr);
+#else
+        const char *fileName = nameNative.c_str();
+#endif
+        if (matchEntry(fileName))
+            return true;
+    }
+    return false;
 }
 
 bool FindFirstRec::open()
 {
-    if (!dirStream)
-        return (dirStream = opendir(searchDir.c_str()));
+    if (!streamValid())
+    {
+        std::error_code ec;
+        dirStream = {searchDir, ec};
+        return !ec && streamValid();
+    }
     return false;
 }
 
 void FindFirstRec::close()
 {
-    if (dirStream)
-        closedir(dirStream), dirStream = 0;
+    dirStream = {};
 }
 
 bool FindFirstRec::setPath(const char* pathname)
@@ -96,7 +122,12 @@ bool FindFirstRec::setPath(const char* pathname)
     if (pathname && *pathname)
     {
         searchDir = pathname;
+#ifdef _WIN32
+        // Convert directory separators temporary, but keep the drive letter.
+        path_dos2unix(searchDir, false);
+#else
         path_dos2unix(searchDir);
+#endif
         // Win32's FindFirst is designed to reject paths ending with a
         // separator. But legacy code unaware of Unix separators may be unable
         // to remove it and call findfirst with such a pathname. Therefore,
@@ -121,25 +152,28 @@ bool FindFirstRec::setPath(const char* pathname)
                 wildcard = '*';
         }
         // At this point, searchDir always ends with a '/'.
+#ifdef _WIN32
+        path_unix2dos(searchDir);
+#endif
         return true;
     }
     return false;
 }
 
-bool FindFirstRec::matchEntry(struct dirent* e)
+bool FindFirstRec::matchEntry(const char *name)
 {
     struct stat st;
-    if (wildcardMatch(wildcard.c_str(), e->d_name) &&
-        stat((searchDir + e->d_name).c_str(), &st) == 0)
+    if (wildcardMatch(wildcard.c_str(), name) &&
+        stat((searchDir + name).c_str(), &st) == 0)
     {
-        unsigned int fileAttr = cvtAttr(&st, e->d_name);
+        unsigned int fileAttr = cvtAttr(&st, name);
         if (attrMatch(fileAttr))
         {
             // Match found, fill finfo.
             finfo->size = st.st_size;
             finfo->attrib = fileAttr;
             cvtTime(&st, finfo);
-            strnzcpy(finfo->name, e->d_name, sizeof(find_t::name));
+            strnzcpy(finfo->name, name, sizeof(find_t::name));
             return true;
         }
     }
@@ -190,8 +224,10 @@ unsigned int FindFirstRec::cvtAttr(struct stat *st, const char* filename)
         attr |= _A_SUBDIR;
     else if (!(st->st_mode & S_IFREG)) // If not a regular file
         attr |= _A_SYSTEM;
+#ifdef __unix__
     else if (!(st->st_mode & S_IWUSR)) // If no write access, innacurate.
         attr |= _A_RDONLY;
+#endif
     return attr;
 }
 
@@ -210,10 +246,10 @@ void FindFirstRec::cvtTime(struct stat *st, struct find_t *fileinfo)
     } *wr_time = (FatTime *) &fileinfo->wr_time;
 
     struct tm *lt = localtime(&st->st_mtime);
-    *wr_date = { .day   = ushort (lt->tm_mday),
-                 .month = ushort (lt->tm_mon + 1),
-                 .year  = ushort (lt->tm_year - 80) };
-    *wr_time = { .sec   = ushort (lt->tm_sec/2),
-                 .min   = ushort (lt->tm_min),
-                 .hour  = ushort (lt->tm_hour) };
+    *wr_date = { ushort (lt->tm_mday),
+                 ushort (lt->tm_mon + 1),
+                 ushort (lt->tm_year - 80) };
+    *wr_time = { ushort (lt->tm_sec/2),
+                 ushort (lt->tm_min),
+                 ushort (lt->tm_hour) };
 }

commit a24182db8d75b678050d37c23a8c9f7e261b517d

DOS: fix switching to a drive other than C:
Regressed by ceed515e7242bb4874831add9d2cce7efac28c9a.

source/tvision/tdircoll.cpp
@@ -38,7 +38,7 @@
 
 Boolean driveValid( char drive ) noexcept
 {
-#ifdef _WIN32
+#if defined( __BORLANDC__ ) || defined( _WIN32 )
 #if !defined( __FLAT__ )
 I       MOV     AH, 19H     // Save the current drive in BL
 I       INT     21H