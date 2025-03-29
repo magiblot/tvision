db196c765d6ae30eb660f48ae9db10cec50622a7
magiblot <magiblot@hotmail.com>
2025-03-29 01:00:39 +0100

    Check for UTF-8 locale in TText tests
    See https://github.com/magiblot/tvision/issues/176

--- test/platform/ttext.test.cpp
+++ test/platform/ttext.test.cpp
@@ -1,7 +1,10 @@
 #define Uses_TText
 #include <tvision/tv.h>
+#include <tvision/internal/platform.h>
+#include <tvision/internal/unixcon.h>
 
 #include <test.h>
+#include <stdlib.h>
 
 #define COMBINING_ZIGZAG_UTF8 "\xCD\x9B"
 #define COMBINING_ZIGZAG_UTF32 U"\u035B"
@@ -23,8 +26,46 @@ static std::ostream &operator<<(std::ostream &os, TSpan<const char32_t> span)
     return os;
 }
 
+static bool mustSkipTTextTests = false;
+
+#if !defined(_WIN32)
+
+// On Unix, the 'wcwidth' function might be used to determine character widths.
+// The behaviour of this function depends on the current locale, so we need
+// to check if a UTF-8 locale is being used.
+TEST(TText, Utf8LocaleIsBeingUsed)
+{
+    // 'Platform::charWidth' initializes the locale when first invoked.
+    tvision::Platform::charWidth(L'\0');
+    // Then, ensure the UnixConsoleAdapter implementation is used, which
+    // relies on 'wcwidth'.
+    tvision::Platform::charWidth = &tvision::UnixConsoleAdapter::charWidth;
+
+    TStringView input = "☺";
+    wchar_t actual = L'\0';
+
+    // If the locale is right, this should convert from UTF-8 into UTF-32.
+    mbtowc(&actual, &input[0], input.size());
+
+    wchar_t expected = L'☺';
+    bool localeIsUtf8 = (actual == expected);
+    EXPECT_TRUE(localeIsUtf8) <<
+        "\nIMPORTANT!\n"
+        "A UTF-8 locale is required to run these tests. Please specify a valid "
+        "UTF-8 locale using the LC_CTYPE or LC_ALL environment variable. The "
+        "command 'locale -a' may provide you with a list of available locales.";
+
+    // If the locale is wrong, there is no point in running the other tests.
+    mustSkipTTextTests = !localeIsUtf8;
+}
+
+#endif // _WIN32
+
 TEST(TText, ShouldConvertUtf8ControlCharacters)
 {
+    if (mustSkipTTextTests)
+        GTEST_SKIP();
+
     static const TestCase<TStringView> testCases[] =
     {
         {{"\0", 1}, {"\0", 1}},
@@ -51,6 +92,9 @@ TEST(TText, ShouldConvertUtf8ControlCharacters)
 
 TEST(TText, ShouldConvertUtf32ControlCharacters)
 {
+    if (mustSkipTTextTests)
+        GTEST_SKIP();
+
     static const TestCase<TSpan<const char32_t>, TStringView> testCases[] =
     {
         {{U"\0", 1}, {"\0", 1}},
@@ -78,6 +122,9 @@ TEST(TText, ShouldConvertUtf32ControlCharacters)
 
 TEST(TText, ShouldDrawTextInScreenCells)
 {
+    if (mustSkipTTextTests)
+        GTEST_SKIP();
+
     enum { nCells = 2 };
     static const TestCase<TStringView, std::vector<TStringView>> testCases[] =
     {
