c042d63101619ab204e8ac63ea9435d3341bc17d
magiblot <magiblot@hotmail.com>
2025-03-30 21:36:57 +0200

    Remove debug code in tvexposd.asm
    This reverts the changes made in 8052a274211ca2443ded2a68ec0ab4e448a67afb.

--- source/tvision/tvexposd.asm
+++ source/tvision/tvexposd.asm
@@ -12,11 +12,7 @@
 ;       All Rights Reserved.
 ;
 
-ifndef DEBUG_TVEXPOSD
         PUBLIC  @TView@exposed$qv
-else
-        PUBLIC  @TView@exposedAsm$qnvni
-endif
         INCLUDE TV.INC
 
 ifndef __FLAT__
@@ -32,23 +28,13 @@ endif
 
 CODESEG
 
-ifndef DEBUG_TVEXPOSD
 @TView@exposed$qv PROC
         ARG     thisPtr : DWORD
         LOCAL   Target  : Fptr
-else
-@TView@exposedAsm$qnvni PROC FAR
-        ARG     thisPtr : DWORD, v : DWORD, i : DWORD
-        LOCAL   Target  : Fptr, z : WORD, _z : WORD
-endif
 
 ifndef __FLAT__
         USES    SI,DI
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     [z], 0
-                                        mov     dx, 0
-                                        call    putV
-                                        endif
+
         XOR     AX,AX
         LES     DI, [thisPtr]
         TEST    WORD PTR ES:[DI+TViewState],sfExposed
@@ -57,17 +43,7 @@ ifndef __FLAT__
         JGE   @@2
         CMP     AX,ES:[DI+TViewSizeY]
         JGE   @@2
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, [z]
-                                        mov     [_z], dx
-                                        endif
 @@1:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, [_z]
-                                        mov     [z], dx
-                                        mov     dx, 1
-                                        call    putV
-                                        endif
         XOR     BX,BX
         MOV     CX,ES:[DI+TViewSizeX]
         PUSH    AX
@@ -79,48 +55,20 @@ ifndef __FLAT__
         CMP     AX,ES:[DI+TViewSizeY]
         JL    @@1
 @@2:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 2
-                                        call    putV
-                                        mov     dx, ax
-                                        call    putI
-                                        endif
         MOV     AL,0
         JMP   @@30
 @@3:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 3
-                                        call    putV
-                                        mov     dx, ax
-                                        call    putI
-                                        endif
         MOV     AL,1
         JMP   @@30
 @@8:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 8
-                                        call    putV
-                                        endif
         STC
 @@9:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 9
-                                        call    putV
-                                        endif
         RETN
 @@10:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 10
-                                        call    putV
-                                        endif
         LES     DI,DWORD PTR ES:[DI+TViewOwner]
         CMP     WORD PTR ES:[DI+TGroupBuffer+2],0
         JNE   @@9
 @@11:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 11
-                                        call    putV
-                                        endif
         MOV     [Target.offs],DI
         MOV     [Target.segm],ES
         ADD     AX,ES:[DI+TViewOriginY]
@@ -139,26 +87,14 @@ ifndef __FLAT__
         JGE   @@12
         MOV     BX,ES:[DI+TGroupClipAX]
 @@12:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 12
-                                        call    putV
-                                        endif
         CMP     CX,ES:[DI+TGroupClipBX]
         JLE   @@13
         MOV     CX,ES:[DI+TGroupClipBX]
 @@13:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 13
-                                        call    putV
-                                        endif
         CMP     BX,CX
         JGE   @@8
         LES     DI,ES:[DI+TGroupLast]
 @@20:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 20
-                                        call    putV
-                                        endif
         LES     DI,ES:[DI+TViewNext]
         CMP     DI,[Target.offs]
         JNE   @@21
@@ -166,10 +102,6 @@ ifndef __FLAT__
         CMP     SI,[Target.segm]
         JE    @@10
 @@21:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 21
-                                        call    putV
-                                        endif
         TEST    WORD PTR ES:[DI+TViewState],sfVisible
         JE    @@20
         MOV     SI,ES:[DI+TViewOriginY]
@@ -190,23 +122,14 @@ ifndef __FLAT__
         STC
         RETN
 @@22:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 22
-                                        call    putV
-                                        endif
         CMP     CX,SI
         JLE   @@20
         ADD     SI,ES:[DI+TViewSizeX]
         CMP     CX,SI
         JG    @@23
         MOV     CX,ES:[DI+TViewOriginX]
-@@@20:
         JMP   @@20
 @@23:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 23
-                                        call    putV
-                                        endif
         PUSH    [Target.segm]
         PUSH    [Target.offs]
         PUSH    ES
@@ -223,47 +146,10 @@ ifndef __FLAT__
         POP     ES
         POP     [Target.offs]
         POP     [Target.segm]
-        JC    @@@20
+        JC    @@20
         RETN
 @@30:
-                                        ifdef DEBUG_TVEXPOSD
-                                        mov     dx, 30
-                                        call    putV
-                                        mov     dx, -1
-                                        call    putV
-                                        endif
         RET
-
-ifdef DEBUG_TVEXPOSD
-putI PROC NEAR
-        pushf
-        push di
-        push es
-    	les	di, dword ptr [i]
-    	mov	word ptr es:[di],dx
-        pop es
-        pop di
-        popf
-        retn
-putI ENDP
-
-putV PROC NEAR
-        pushf
-        push si
-        push di
-        push es
-        mov	si, [z]
-    	les	di, dword ptr [v]
-    	add	di, si
-    	mov	word ptr es:[di],dx
-        inc [z]
-        pop es
-        pop di
-        pop si
-        popf
-        retn
-putV ENDP
-endif
 
 else        ;;;;;;;;;;;;;;;;;;;;;;;;; 32-bit version ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -381,4 +267,3 @@ endif
 ENDP
 
 END
-endif
