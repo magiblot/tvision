magiblot/tvision

commit 9fc49efbab156ea87cfd0217fe01837dc4c723e3

Improve TStringView description and add noexcept qualifiers
Also make the comparison operators "hidden friends" to avoid polluting the global namespace unnecessarily, and remove the library feature test macros, since they were not really useful.

include/tvision/tstrview.h
@@ -12,119 +12,140 @@
 #include <string.h>
 #include <tvision/compat/borland/iosfwd.h>
 
-#ifdef TVISION_STL
+#if !defined(TVISION_NO_STL)
 #include <string>
 
-#if __cplusplus >= 201703L || __cpp_lib_string_view
+#if __cplusplus >= 201703L
 #include <string_view>
 #endif
-#endif // TVISION_STL
+#endif // TVISION_NO_STL
+
+/*-------------------------------------------------------------------------*/
+/*                                                                         */
+/*  class TStringView                                                      */
+/*                                                                         */
+/*  Represents a read-only view over a contiguous sequence of characters,  */
+/*  similar to std::string_view. It is used throughout Turbo Vision's API  */
+/*  to pass around text without copying or owning it.                      */
+/*                                                                         */
+/*  If std::string_view is available to you, you can continue to use it    */
+/*  in your own code: TStringView is intercompatible with                  */
+/*  std::string_view, std::string and TSpan<const char>, converting to     */
+/*  and from them implicitly where possible.                               */
+/*                                                                         */
+/*  Unlike std::string_view, TStringView can be constructed from a null    */
+/*  pointer, in which case it results in an empty view. Turbo Vision       */
+/*  originally used 'const char *' parameters everywhere instead.          */
+/*                                                                         */
+/*-------------------------------------------------------------------------*/
 
 class TStringView
 {
+public:
 
-    // This class exists only to compensate for the lack of std::string_view
-    // in Borland C++. Unless you are programming for that compiler, you should
-    // always use std::string_view.
-    // Unlike std::string_view, TStringView can be constructed from a null pointer,
-    // for backward compatibility.
-    // TStringView is intercompatible with std::string_view, std::string and
-    // TSpan<const char>.
+    constexpr TStringView() noexcept;
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201703L
+    constexpr TStringView(const char _FAR *str) noexcept;
+#else
+    TStringView(const char _FAR *str) noexcept;
+#endif
+    constexpr TStringView(const char _FAR *str, size_t len) noexcept;
+    constexpr TStringView(TSpan<char> span) noexcept;
+    constexpr TStringView(TSpan<const char> span) noexcept;
+#if !defined(TVISION_NO_STL)
+#if __cplusplus >= 201703L
+    constexpr TStringView(std::string_view text) noexcept;
+    constexpr operator std::string_view() const noexcept;
+#endif
+    TStringView(const std::string &text) noexcept;
+    operator std::string() const;
+#endif // TVISION_NO_STL
+    constexpr operator TSpan<const char>() const noexcept;
+
+    constexpr const char _FAR * data() const noexcept;
+    constexpr size_t size() const noexcept;
+    constexpr Boolean empty() const noexcept;
+    constexpr const char _FAR & operator[](size_t pos) const noexcept;
+    constexpr const char _FAR & front() const noexcept;
+    constexpr const char _FAR & back() const noexcept;
+
+    constexpr TStringView substr(size_t pos) const noexcept;
+    constexpr TStringView substr(size_t pos, size_t n) const noexcept;
+
+    constexpr const char _FAR * begin() const noexcept;
+    constexpr const char _FAR * cbegin() const noexcept;
+    constexpr const char _FAR * end() const noexcept;
+    constexpr const char _FAR * cend() const noexcept;
+
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201703L
+    friend constexpr Boolean operator==(TStringView a, TStringView b) noexcept;
+    friend constexpr Boolean operator!=(TStringView a, TStringView b) noexcept;
+#else
+    friend Boolean operator==(TStringView a, TStringView b) noexcept;
+    friend Boolean operator!=(TStringView a, TStringView b) noexcept;
+#endif
+
+    friend ostream _FAR & _Cdecl operator<<(ostream _FAR &, TStringView);
+
+private:
 
     const char _FAR *str;
     size_t len;
-
-public:
-
-    constexpr TStringView();
-#if defined(TVISION_STL) && (__cplusplus >= 201703L || __cpp_lib_constexpr_char_traits)
-    constexpr
-#endif
-              TStringView(const char _FAR *str);
-    constexpr TStringView(const char _FAR *str, size_t len);
-    constexpr TStringView(TSpan<char> span);
-    constexpr TStringView(TSpan<const char> span);
-#ifdef TVISION_STL
-#if __cplusplus >= 201703L || __cpp_lib_string_view
-    constexpr TStringView(std::string_view text);
-    constexpr operator std::string_view() const;
-#endif
-    TStringView(const std::string &text);
-    operator std::string() const;
-#endif // TVISION_STL
-    constexpr operator TSpan<const char>() const;
-
-    constexpr const char _FAR * data() const;
-    constexpr size_t size() const;
-    constexpr Boolean empty() const;
-    constexpr const char _FAR & operator[](size_t pos) const;
-    constexpr const char _FAR & front() const;
-    constexpr const char _FAR & back() const;
-
-    constexpr TStringView substr(size_t pos) const;
-    constexpr TStringView substr(size_t pos, size_t n) const;
-
-    constexpr const char _FAR * begin() const;
-    constexpr const char _FAR * cbegin() const;
-    constexpr const char _FAR * end() const;
-    constexpr const char _FAR * cend() const;
-
 };
 
-inline constexpr TStringView::TStringView() :
+constexpr TStringView::TStringView() noexcept :
     str(0),
     len(0)
 {
 }
 
-#if defined(TVISION_STL) && (__cplusplus >= 201703L || __cpp_lib_constexpr_char_traits)
-constexpr
-inline TStringView::TStringView(const char _FAR *str) :
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201703L
+constexpr TStringView::TStringView(const char _FAR *str) noexcept :
     str(str),
     len(str ? std::char_traits<char>::length(str) : 0)
 {
 }
 #else
-inline TStringView::TStringView(const char _FAR *str) :
+inline TStringView::TStringView(const char _FAR *str) noexcept :
     str(str),
     len(str ? strlen(str) : 0)
 {
 }
 #endif
 
-inline constexpr TStringView::TStringView(const char _FAR *str, size_t len) :
+constexpr TStringView::TStringView(const char _FAR *str, size_t len) noexcept :
     str(str),
     len(len)
 {
 }
 
-inline constexpr TStringView::TStringView(TSpan<char> span) :
+constexpr TStringView::TStringView(TSpan<char> span) noexcept :
     str(span.data()),
     len(span.size())
 {
 }
 
-inline constexpr TStringView::TStringView(TSpan<const char> span) :
+constexpr TStringView::TStringView(TSpan<const char> span) noexcept :
     str(span.data()),
     len(span.size())
 {
 }
 
-#ifdef TVISION_STL
-#if __cplusplus >= 201703L || __cpp_lib_string_view
-inline constexpr TStringView::TStringView(std::string_view text) :
+#if !defined(TVISION_NO_STL)
+#if __cplusplus >= 201703L
+constexpr TStringView::TStringView(std::string_view text) noexcept :
     str(text.data()),
     len(text.size())
 {
 }
 
-inline constexpr TStringView::operator std::string_view() const
+constexpr TStringView::operator std::string_view() const noexcept
 {
     return {str, len};
 }
 #endif
 
-inline TStringView::TStringView(const std::string &text) :
+inline TStringView::TStringView(const std::string &text) noexcept :
     str(text.data()),
     len(text.size())
 {
@@ -134,44 +155,44 @@ inline TStringView::operator std::string() const
 {
     return {str, len};
 }
-#endif // TVISION_STL
+#endif // TVISION_NO_STL
 
-inline constexpr TStringView::operator TSpan<const char>() const
+constexpr TStringView::operator TSpan<const char>() const noexcept
 {
     return TSpan<const char>(str, len);
 }
 
-inline constexpr const char _FAR * TStringView::data() const
+constexpr const char _FAR * TStringView::data() const noexcept
 {
     return str;
 }
 
-inline constexpr size_t TStringView::size() const
+constexpr size_t TStringView::size() const noexcept
 {
     return len;
 }
 
-inline constexpr Boolean TStringView::empty() const
+constexpr Boolean TStringView::empty() const noexcept
 {
     return Boolean( size() == 0 );
 }
 
-inline constexpr const char _FAR & TStringView::operator[](size_t pos) const
+constexpr const char _FAR & TStringView::operator[](size_t pos) const noexcept
 {
     return str[pos];
 }
 
-inline constexpr const char _FAR & TStringView::front() const
+constexpr const char _FAR & TStringView::front() const noexcept
 {
     return str[0];
 }
 
-inline constexpr const char _FAR & TStringView::back() const
+constexpr const char _FAR & TStringView::back() const noexcept
 {
     return str[len - 1];
 }
 
-inline constexpr TStringView TStringView::substr(size_t pos) const
+constexpr TStringView TStringView::substr(size_t pos) const noexcept
 {
     if (pos >= len)
         return TStringView(str + len, 0);
@@ -179,7 +200,7 @@ inline constexpr TStringView TStringView::substr(size_t pos) const
         return TStringView(str + pos, len - pos);
 }
 
-inline constexpr TStringView TStringView::substr(size_t pos, size_t n) const
+constexpr TStringView TStringView::substr(size_t pos, size_t n) const noexcept
 {
     if (pos >= len)
         return TStringView(str + len, 0);
@@ -187,35 +208,35 @@ inline constexpr TStringView TStringView::substr(size_t pos, size_t n) const
         return TStringView(str + pos, n <= len - pos ? n : len - pos);
 }
 
-inline constexpr const char _FAR * TStringView::begin() const
+constexpr const char _FAR * TStringView::begin() const noexcept
 {
     return &str[0];
 }
 
-inline constexpr const char _FAR * TStringView::cbegin() const
+constexpr const char _FAR * TStringView::cbegin() const noexcept
 {
     return &str[0];
 }
 
-inline constexpr const char _FAR * TStringView::end() const
+constexpr const char _FAR * TStringView::end() const noexcept
 {
     return &str[len];
 }
 
-inline constexpr const char _FAR * TStringView::cend() const
+constexpr const char _FAR * TStringView::cend() const noexcept
 {
     return &str[len];
 }
 
-#if defined(TVISION_STL) && (__cplusplus >= 201703L || __cpp_lib_constexpr_char_traits)
-inline constexpr Boolean operator==(TStringView a, TStringView b)
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201703L
+constexpr Boolean operator==(TStringView a, TStringView b) noexcept
 {
     if (a.size() == b.size())
         return Boolean( std::char_traits<char>::compare(a.data(), b.data(), b.size()) == 0 );
     return False;
 }
 #else
-inline Boolean operator==(TStringView a, TStringView b)
+inline Boolean operator==(TStringView a, TStringView b) noexcept
 {
     if (a.size() == b.size())
         return Boolean( b.size() == 0 || memcmp(a.data(), b.data(), b.size()) == 0 );
@@ -223,32 +244,28 @@ inline Boolean operator==(TStringView a, TStringView b)
 }
 #endif
 
-#if defined(TVISION_STL) && (__cplusplus >= 201703L || __cpp_lib_constexpr_char_traits)
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201703L
 constexpr
 #endif
-inline Boolean operator!=(TStringView a, TStringView b)
+inline Boolean operator!=(TStringView a, TStringView b) noexcept
 {
     return Boolean( !(a == b) );
 }
 
-#if defined(TVISION_STL) && __cplusplus >= 201103L
+#if !defined(TVISION_NO_STL) && __cplusplus >= 201103L
 
 #include <typeindex>
 
 namespace std {
-#if __cplusplus >= 201703L || __cpp_lib_string_view
+#if __cplusplus >= 201703L
     template<>
-    struct hash<TStringView> : public std::hash<std::string_view>
-    {
-    };
+    struct hash<TStringView> : public hash<std::string_view> {};
 #else
     template<>
-    struct hash<TStringView> : public std::hash<std::string>
-    {
-    };
+    struct hash<TStringView> : public hash<std::string> {};
 #endif
 } // namespace std
 
-#endif // TVISION_STL && __cplusplus >= 201103L
+#endif // TVISION_NO_STL && __cplusplus >= 201103L
 
 #endif // TVISION_TSTRVIEW_H

include/tvision/ttypes.h
@@ -102,10 +102,6 @@ opstream& _Cdecl operator << ( opstream&, long double );
 opstream& _Cdecl operator << ( opstream&, TStreamable& );
 opstream& _Cdecl operator << ( opstream&, TStreamable _FAR * );
 
-#include <tvision/compat/borland/iosfwd.h>
-class TStringView;
-ostream _FAR & _Cdecl operator<<(ostream _FAR &, TStringView);
-
 typedef void _FAR *TTimerId;
 
 typedef int ccIndex;
@@ -125,12 +121,6 @@ extern const uchar specialChars[];
 #define constexpr
 #define noexcept
 #define thread_local
-#endif
-
-// Do not include unnecessary STL headers if TVISION_NO_STL is defined.
-// This speeds up compilation when building the library.
-#if !defined( __BORLANDC__ ) && !defined( TVISION_NO_STL )
-#define TVISION_STL
 #endif
 
 #endif  // __TTYPES_H

include/tvision/util.h
@@ -46,6 +46,7 @@ inline constexpr const T& max( const T& a, const T& b )
 void fexpand( char *rpath ) noexcept;
 void fexpand( char *rpath, const char *relativeTo ) noexcept;
 
+class TStringView;
 char hotKey( TStringView s ) noexcept;
 ushort ctrlToArrow( ushort keyCode ) noexcept;
 char getAltChar( ushort keyCode ) noexcept;
@@ -90,6 +91,8 @@ Boolean isWild( const char *f ) noexcept;
 
 size_t strnzcpy( char *dst, TStringView src, size_t dstSize ) noexcept;
 size_t strnzcat( char *dst, TStringView src, size_t dstSize ) noexcept;
+
+#include <tvision/compat/borland/iosfwd.h>
 
 void printKeyCode(ostream _FAR &, ushort keyCode);
 void printControlKeyState(ostream _FAR &, ushort controlKeyState);

commit 4f603dab61b0f2906d62cfd19e5f1c9af3f676b8

Fix workaround for 'constexpr' keyword on Borland C++
Regressed by 9fc49efbab156ea87cfd0217fe01837dc4c723e3, which replaced 'inline constexpr' with just 'constexpr' (because it implies 'inline').

include/tvision/tkeys.h
@@ -185,20 +185,20 @@ public:
     ushort mods;
 };
 
-inline constexpr TKey::TKey() noexcept :
+constexpr TKey::TKey() noexcept :
     code(0),
     mods(0)
 {
 }
 
-inline constexpr Boolean operator==(TKey a, TKey b) noexcept
+constexpr Boolean operator==(TKey a, TKey b) noexcept
 {
     return Boolean(
         (a.code | ((int32_t) a.mods << 16)) == (b.code | ((int32_t) b.mods << 16))
     );
 }
 
-inline constexpr Boolean operator!=(TKey a, TKey b) noexcept
+constexpr Boolean operator!=(TKey a, TKey b) noexcept
 {
     return Boolean( !(a == b) );
 }

include/tvision/tstrview.h
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <tvision/compat/borland/iosfwd.h>
 
-#if !defined(TVISION_NO_STL)
+#if !defined(TVISION_NO_STL) && __cplusplus >= 199711L
 #include <string>
 
 #if __cplusplus >= 201703L
@@ -52,7 +52,7 @@ public:
     constexpr TStringView(const char _FAR *str, size_t len) noexcept;
     constexpr TStringView(TSpan<char> span) noexcept;
     constexpr TStringView(TSpan<const char> span) noexcept;
-#if !defined(TVISION_NO_STL)
+#if !defined(TVISION_NO_STL) && __cplusplus >= 199711L
 #if __cplusplus >= 201703L
     constexpr TStringView(std::string_view text) noexcept;
     constexpr operator std::string_view() const noexcept;
@@ -131,7 +131,7 @@ constexpr TStringView::TStringView(TSpan<const char> span) noexcept :
 {
 }
 
-#if !defined(TVISION_NO_STL)
+#if !defined(TVISION_NO_STL) && __cplusplus >= 199711L
 #if __cplusplus >= 201703L
 constexpr TStringView::TStringView(std::string_view text) noexcept :
     str(text.data()),

include/tvision/ttypes.h
@@ -118,7 +118,7 @@ extern const uchar specialChars[];
 
 // Reserve future keywords
 #if __cplusplus < 201103L
-#define constexpr
+#define constexpr inline
 #define noexcept
 #define thread_local
 #endif

include/tvision/util.h
@@ -18,12 +18,12 @@
 
 #include <stdarg.h>
 
-inline constexpr int min( int a, int b )
+constexpr int min( int a, int b )
 {
     return a < b ? a : b;
 }
 
-inline constexpr int max( int a, int b )
+constexpr int max( int a, int b )
 {
     return a > b ? a : b;
 }
@@ -31,13 +31,13 @@ inline constexpr int max( int a, int b )
 #if !defined( __MINMAX_DEFINED ) // Also defined in Borland C++'s stdlib.h.
 #define __MINMAX_DEFINED
 template <class T>
-inline constexpr const T& min( const T& a, const T& b )
+constexpr const T& min( const T& a, const T& b )
 {
     return a < b ? a : b;
 }
 
 template <class T>
-inline constexpr const T& max( const T& a, const T& b )
+constexpr const T& max( const T& a, const T& b )
 {
     return a > b ? a : b;
 }