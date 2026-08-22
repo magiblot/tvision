magiblot/tvision

commit 00799727cd84893915f69fb3e36052f273965de1

TEditor: improve nextWord/prevWord behaviour

source/tvision/teditor2.cpp
@@ -41,9 +41,9 @@ uint scan( const char *block, uint size, const char *str );
 uint iScan( const char *block, uint size, const char *str );
 };
 
-inline int isWordChar( int ch )
+static inline int isWordChar( int ch )
 {
-    return isalnum(ch) || ch == '_';
+    return strchr(" !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", ch) == 0;
 }
 
 #pragma warn -asc

commit 7b21b2217a13f20f89df33b093c66e81ab7f0b0f

TEditor: properly skip spaces in nextWord/prevWord
Fixes https://github.com/magiblot/tvision/issues/193
Also fixes a regression on whole word search introduced by 00799727cd84893915f69fb3e36052f273965de1

include/tvision/editors.h
@@ -310,6 +310,8 @@ private:
     TStringView getLineEnding();
     uint lengthWithConvertedLineEndings( const char *, uint );
     void copyAndConvertLineEndings( char *, const char *, uint );
+    uint nextWordBoundary( uint );
+    uint prevWordBoundary( uint );
 
     static const LineEndingType defaultLineEndingType;
 

source/tvision/teditor2.cpp
@@ -42,25 +42,30 @@ uint scan( const char *block, uint size, const char *str );
 uint iScan( const char *block, uint size, const char *str );
 }
 
-static int getCharType( char ch )
+static int getCharCategory( char ch )
 {
-    if( strchr("\t \0", ch) )
+    if( ch == ' ' || ch == '\t' || ch == '\0' )
         return 0;
-    if( strchr("\n\r", ch) )
+    if( ch == '\n' || ch == '\r' )
         return 1;
-    if( strchr("!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", ch) )
+    if( ch != '_' && ispunct((uchar) ch) )
         return 2;
     return 3;
 }
 
 static inline int isWordBoundary( char a, char b )
 {
-    return getCharType(a) != getCharType(b);
+    return getCharCategory(a) != getCharCategory(b);
 }
 
-static inline int isWordChar( int ch )
+static inline int isWhitespaceOrNewline( char ch )
 {
-    return strchr(" !\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~\0", ch) == 0;
+    return getCharCategory(ch) <= 1;
+}
+
+static inline int isWordChar( char ch )
+{
+    return getCharCategory(ch) == 3;
 }
 
 TEditor::LineEndingType TEditor::detectLineEndingType()
@@ -315,17 +320,23 @@ uint TEditor::nextLine( uint p )
     return nextChar(lineEnd(p));
 }
 
-uint TEditor::nextWord( uint p )
+uint TEditor::nextWordBoundary( uint p )
 {
     if (p < bufLen)
         {
-        char a = bufChar(p);
-        char b;
+        char c = bufChar(p);
         do  {
-            b = a;
             p = nextChar(p);
-            } while( p < bufLen && !isWordBoundary((a = bufChar(p)), b) );
+            } while( p < bufLen && !isWordBoundary(bufChar(p), c) );
         }
+    return p;
+}
+
+uint TEditor::nextWord( uint p )
+{
+    p = nextWordBoundary(p);
+    while( p < bufLen && isWhitespaceOrNewline(bufChar(p)) )
+        p = nextWordBoundary(p);
     return p;
 }
 
@@ -334,20 +345,28 @@ uint TEditor::prevLine( uint p )
   return lineStart(prevChar(p));
 }
 
+uint TEditor::prevWordBoundary( uint p )
+{
+    if (p > 0)
+        {
+        p = prevChar(p);
+        char c = bufChar(p);
+        while( p > 0 )
+            {
+            uint q = prevChar(p);
+            if( isWordBoundary(bufChar(q), c) )
+                break;
+            p = q;
+            }
+        }
+    return p;
+}
+
 uint TEditor::prevWord( uint p )
 {
-    if (p > 0 && (p = prevChar(p), p > 0))
-        {
-        char a = bufChar(p);
-        char b;
-        do  {
-            b = a;
-            p = prevChar(p);
-            a = bufChar(p);
-            } while( p > 0 && !isWordBoundary(a, b) );
-        if( isWordBoundary(a, b) )
-            p = nextChar(p);
-        }
+    p = prevWordBoundary(p);
+    while( p > 0 && isWhitespaceOrNewline(bufChar(p)) )
+        p = prevWordBoundary(p);
     return p;
 }
 
@@ -470,8 +489,8 @@ void TEditor::setCurPtr( uint p, uchar selectMode )
         {
         if( (selectMode & smDouble) != 0 )
             {
-            p = prevWord(nextWord(p));
-            anchor = nextWord(prevWord(anchor));
+            p = prevWordBoundary(nextWordBoundary(p));
+            anchor = nextWordBoundary(prevWordBoundary(anchor));
             }
         else if( (selectMode & smTriple) != 0 )
             {
@@ -484,8 +503,8 @@ void TEditor::setCurPtr( uint p, uchar selectMode )
         {
         if( (selectMode & smDouble) != 0 )
             {
-            p = nextWord(p);
-            anchor = prevWord(nextWord(anchor));
+            p = nextWordBoundary(p);
+            anchor = prevWordBoundary(nextWordBoundary(anchor));
             }
         else if( (selectMode & smTriple) != 0 )
             {

test/platform/termio.test.cpp
@@ -21,7 +21,7 @@ TEST(TermIO, ShouldNormalizeKeys)
         {keyDownEv(kbIns, kbShift), keyDownEv(kbShiftIns, kbShift)},
         {keyDownEv(kbBack, kbLeftCtrl | kbLeftAlt), keyDownEv(kbAltBack, kbLeftCtrl | kbLeftAlt)},
         {keyDownEv(kbCtrlBack, kbRightCtrl), keyDownEv(kbCtrlBack, kbRightCtrl)},
-        {keyDownEv(kbCtrlBack, 0x0000), keyDownEv(kbCtrlBack, kbLeftCtrl)},
+        {keyDownEv(kbCtrlBack), keyDownEv(kbCtrlBack, kbLeftCtrl)},
         {keyDownEv(kbIns, kbLeftCtrl | kbEnhanced), keyDownEv(kbCtrlIns, kbLeftCtrl | kbEnhanced)},
         {keyDownEv(kbCtrlDel, kbLeftAlt), keyDownEv(kbAltDel, kbLeftCtrl | kbLeftAlt)},
     };
@@ -168,19 +168,19 @@ TEST(TermIO, ShouldReadKittyKeys)
         // ф + Shift + Alt (RU)
         {"\x1B[1092:1060:97;4u", {keyDownEv(kbAltA, kbShift | kbLeftAlt, "Ф")}},
         // Right
-        {"\x1B[C", {keyDownEv(kbRight, 0x0000)}},
+        {"\x1B[C", {keyDownEv(kbRight)}},
         // F2
-        {"\x1B[Q", {keyDownEv(kbF2, 0x0000)}},
+        {"\x1B[Q", {keyDownEv(kbF2)}},
         // F3
-        {"\x1B[13~", {keyDownEv(kbF3, 0x0000)}},
+        {"\x1B[13~", {keyDownEv(kbF3)}},
         // Del
-        {"\x1B[3~", {keyDownEv(kbDel, 0x0000)}},
+        {"\x1B[3~", {keyDownEv(kbDel)}},
         // Tab
-        {"\x1B[9u", {keyDownEv(kbTab, 0x0000)}},
+        {"\x1B[9u", {keyDownEv(kbTab)}},
         // Enter
-        {"\x1B[13u", {keyDownEv(kbEnter, 0x0000)}},
+        {"\x1B[13u", {keyDownEv(kbEnter)}},
         // Back
-        {"\x1B[127u", {keyDownEv(kbBack, 0x0000)}},
+        {"\x1B[127u", {keyDownEv(kbBack)}},
         // Space + Ctrl
         {"\x1B[32;5u", {keyDownEv(0x0020, kbLeftCtrl, " ")}},
         // Enter + Ctrl

test/test_group.h
@@ -1,0 +1,48 @@
+#ifndef TVISION_TEST_GROUP_H
+#define TVISION_TEST_GROUP_H
+
+#define Uses_TEvent
+#define Uses_TGroup
+#include <tvision/tv.h>
+
+#include <forward_list>
+#include <test_operators.h>
+
+// A TGroup whose getEvent()/putEvent() are backed by a queue, so that code
+// relying on event loops can be driven deterministically in tests. Once the
+// queue is exhausted, 'fallbackEvent' is returned on every subsequent getEvent().
+class TestGroup : public TGroup
+{
+public:
+
+    TestGroup( std::forward_list<TEvent> aEventQueue = {},
+               TEvent aFallbackEvent = {} ) :
+        TGroup(TRect()),
+        eventQueue(std::move(aEventQueue)),
+        fallbackEvent(aFallbackEvent)
+    {
+    }
+
+    void getEvent(TEvent &event) override
+    {
+        if (!eventQueue.empty())
+        {
+            event = eventQueue.front();
+            eventQueue.pop_front();
+        }
+        else
+            event = fallbackEvent;
+    }
+
+    void putEvent(TEvent &event) override
+    {
+        eventQueue.push_front(event);
+    }
+
+private:
+
+    std::forward_list<TEvent> eventQueue;
+    TEvent fallbackEvent;
+};
+
+#endif // TVISION_TEST_GROUP_H

test/test_operators.h
@@ -83,7 +83,7 @@ inline std::ostream &operator<<(std::ostream &os, TPoint p)
     return os;
 }
 
-inline TEvent keyDownEv(ushort keyCode, ushort controlKeyState, TStringView text = {})
+inline TEvent keyDownEv(ushort keyCode, ushort controlKeyState = 0, TStringView text = {})
 {
     TEvent ev {};
     ev.what = evKeyDown;
@@ -106,6 +106,20 @@ inline TEvent mouseEv(TPoint where, ushort eventFlags, ushort controlKeyState, u
     ev.mouse.controlKeyState = controlKeyState;
     ev.mouse.buttons = buttons;
     ev.mouse.wheel = wheel;
+    return ev;
+}
+
+inline TEvent mouseDownEv(TPoint where, ushort eventFlags = 0, uchar buttons = mbLeftButton, ushort controlKeyState = 0, uchar wheel = 0)
+{
+    TEvent ev = mouseEv(where, eventFlags, controlKeyState, buttons, wheel);
+    ev.what = evMouseDown;
+    return ev;
+}
+
+inline TEvent mouseUpEv(TPoint where, uchar buttons = mbLeftButton, ushort controlKeyState = 0, uchar wheel = 0)
+{
+    TEvent ev = mouseEv(where, 0, controlKeyState, buttons, wheel);
+    ev.what = evMouseUp;
     return ev;
 }
 

test/tvision/teditor.test.cpp
@@ -3,9 +3,11 @@
 #define Uses_TText
 #include <tvision/tv.h>
 
+#include <forward_list>
 #include <test.h>
 #include <test_operators.h>
 #include <test_charops.h>
+#include <test_group.h>
 
 struct TEditorBufferTestInput
 {
@@ -19,12 +21,13 @@ struct TEditorBufferTestOutput
     int lineEndingType;
 };
 
-class DrawableTestGroup : public TGroup
+class DrawableTestGroup : public TestGroup
 {
 public:
-    DrawableTestGroup(const TRect &bounds) noexcept :
-        TGroup(bounds)
+    DrawableTestGroup(const TRect &bounds, std::forward_list<TEvent> eventQueue = {}) noexcept :
+        TestGroup(std::move(eventQueue))
     {
+        changeBounds(bounds);
     }
 
     TColorAttr mapColor(uchar i) noexcept
@@ -68,7 +71,7 @@ struct TEditorDisplayTestInput
 {
     TPoint size;
     TStringView text;
-    std::vector<TEvent> events;
+    std::forward_list<TEvent> events;
     TPoint delta;
 };
 
@@ -237,10 +240,11 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         // Remove part of a multi-byte character, then undo the removal.
         {   {   TPoint { 3, 1 },
                 "€",
-                {   messageEv(evCommand, cmEncoding),
-                    keyDownEv(kbLeft, 0x0000),
-                    keyDownEv(kbBack, 0x0000),
-                    keyDownEv(kbBack, 0x0000),
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmEncoding),
+                    keyDownEv(kbLeft),
+                    keyDownEv(kbBack),
+                    keyDownEv(kbBack),
                     messageEv(evCommand, cmEncoding),
                     messageEv(evCommand, cmUndo),
                 },
@@ -256,6 +260,7 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         {   {   TPoint { 4, 1 },
                 "€",
                 {   messageEv(evCommand, cmEncoding),
+                    messageEv(evCommand, cmTextEnd),
                 },
                 TPoint { 0, 0 },
             },
@@ -268,7 +273,8 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         // Tabulators should be properly drawn when there is horizontal scroll.
         {   {   TPoint { 3, 1 },
                 "\ta",
-                {},
+                {   messageEv(evCommand, cmTextEnd),
+                },
                 TPoint { 6, 0 },
             },
             {   {{ " ", " ", "a" }},
@@ -280,7 +286,8 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         // A partially visible tabulator should be drawn properly when selected.
         {   {   TPoint { 3, 1 },
                 "\ta",
-                {   keyDownEv(kbLeft, 0x0000),
+                {   messageEv(evCommand, cmTextEnd),
+                    keyDownEv(kbLeft),
                     keyDownEv(kbLeft, kbShift),
                 },
                 TPoint { 6, 0 },
@@ -294,16 +301,16 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         // A selected line break should be drawn properly.
         {   {   TPoint { 3, 2 },
                 "ab\ncd",
-                {   keyDownEv(kbLeft, 0x0000),
-                    keyDownEv(kbLeft, kbShift),
-                    keyDownEv(kbLeft, kbShift),
-                    keyDownEv(kbLeft, kbShift),
+                {   keyDownEv(kbRight),
+                    keyDownEv(kbRight, kbShift),
+                    keyDownEv(kbRight, kbShift),
+                    keyDownEv(kbRight, kbShift),
                 },
                 TPoint { 0, 0 },
             },
             {   {{ "a", "b", " " }, { "c", "d", " " }},
                 {{   6,   7,   7 }, {   7,   6,   6 }},
-                TPoint { 1, 0 },
+                TPoint { 1, 1 },
                 TPoint { 0, 0 },
             },
         },
@@ -315,14 +322,15 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
             },
             {   {{ "😊", "(wide char trail)", "a" }},
                 {{   6,   6,   6 }},
-                TPoint { 3, 0 },
+                TPoint { 0, 0 },
                 TPoint { 0, 0 },
             },
         },
         // A partially visible and selected double-width character should be drawn properly.
         {   {   TPoint { 2, 1 },
                 SMILING_FACE_WITH_SMILING_EYES_UTF8 "a",
-                {   keyDownEv(kbLeft, 0x0000),
+                {   messageEv(evCommand, cmTextEnd),
+                    keyDownEv(kbLeft),
                     keyDownEv(kbLeft, kbShift),
                 },
                 TPoint { 1, 0 },
@@ -341,8 +349,205 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
             },
             {   {{ "b" COMBINING_ZIGZAG_UTF8, "c" COMBINING_ZIGZAG_UTF8 }},
                 {{   6,   6 }},
-                TPoint { 4, 0 },
+                TPoint { 0, 0 },
                 TPoint { 1, 0 },
+            },
+        },
+        // Ctrl+Right should skip trailing whitespace instead of stopping on it.
+        {   {   TPoint { 7, 1 },
+                "foo bar",
+                {   messageEv(evCommand, cmWordRight),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 4, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Left should do the same in reverse.
+        {   {   TPoint { 7, 1 },
+                "foo bar",
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmWordLeft),
+                    messageEv(evCommand, cmWordLeft),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 0, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Right should reach the punctuation before the next word.
+        {   {   TPoint { 10, 1 },
+                "foo(); bar",
+                {   messageEv(evCommand, cmWordRight),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "(", ")", ";", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 3, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // A second Ctrl+Right should then skip the punctuation and the whitespace after it together.
+        {   {   TPoint { 10, 1 },
+                "foo(); bar",
+                {   messageEv(evCommand, cmWordRight),
+                    messageEv(evCommand, cmWordRight),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "(", ")", ";", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 7, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Left should reach the same two points in reverse.
+        {   {   TPoint { 10, 1 },
+                "foo(); bar",
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmWordLeft),
+                    messageEv(evCommand, cmWordLeft),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "(", ")", ";", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 3, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        {   {   TPoint { 10, 1 },
+                "foo(); bar",
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmWordLeft),
+                    messageEv(evCommand, cmWordLeft),
+                    messageEv(evCommand, cmWordLeft),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "(", ")", ";", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 0, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Right should skip consecutive blank lines in a single jump.
+        {   {   TPoint { 3, 4 },
+                "foo\n\n\nbar",
+                {   messageEv(evCommand, cmWordRight),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o" }, { " ", " ", " " }, { " ", " ", " " }, { "b", "a", "r" }},
+                {{   6,   6,   6 }, {   6,   6,   6 }, {   6,   6,   6 }, {   6,   6,   6 }},
+                TPoint { 0, 3 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Left should do the same in reverse.
+        {   {   TPoint { 3, 4 },
+                "foo\n\n\nbar",
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmWordLeft),
+                    messageEv(evCommand, cmWordLeft),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o" }, { " ", " ", " " }, { " ", " ", " " }, { "b", "a", "r" }},
+                {{   6,   6,   6 }, {   6,   6,   6 }, {   6,   6,   6 }, {   6,   6,   6 }},
+                TPoint { 0, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Del should delete consecutive punctuation characters and the whitespace after them in one step.
+        {   {   TPoint { 10, 1 },
+                "foo(); bar",
+                {   messageEv(evCommand, cmWordRight),
+                    messageEv(evCommand, cmDelWord),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "b", "a", "r", " ", " ", " ", " " }},
+                {{   6,   6,   6,   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 3, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Ctrl+Backspace should mirror that going left.
+        {   {   TPoint { 7, 1 },
+                "foo bar",
+                {   messageEv(evCommand, cmTextEnd),
+                    messageEv(evCommand, cmDelWordLeft),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " ", " ", " ", " " }},
+                {{   6,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 4, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Double-clicking inside a word selects exactly that word.
+        {   {   TPoint { 9, 1 },
+                "foo   bar",
+                {   mouseDownEv({ 1, 0 }, meDoubleClick),
+                    mouseUpEv({ 1, 0 }),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " ", " ", " ", "b", "a", "r" }},
+                {{   7,   7,   7,   6,   6,   6,   6,   6,   6 }},
+                TPoint { 3, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Double-clicking inside a run of whitespace selects just that run.
+        {   {   TPoint { 9, 1 },
+                "foo   bar",
+                {   mouseDownEv({ 4, 0 }, meDoubleClick),
+                    mouseUpEv({ 4, 0 }),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " ", " ", " ", "b", "a", "r" }},
+                {{   6,   6,   6,   7,   7,   7,   6,   6,   6 }},
+                TPoint { 6, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Double-clicking on punctuation selects just the consecutive punctuation characters.
+        {   {   TPoint { 9, 1 },
+                "foo();bar",
+                {   mouseDownEv({ 4, 0 }, meDoubleClick),
+                    mouseUpEv({ 4, 0 }),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", "(", ")", ";", "b", "a", "r" }},
+                {{   6,   6,   6,   7,   7,   7,   6,   6,   6 }},
+                TPoint { 6, 0 },
+                TPoint { 0, 0 },
+            },
+        },
+        // Double-clicking on trailing whitespace must not select across a line break.
+        {   {   TPoint { 4, 2 },
+                "foo \n bar",
+                {   mouseDownEv({ 3, 0 }, meDoubleClick),
+                    mouseUpEv({ 3, 0 }),
+                },
+                TPoint { 0, 0 },
+            },
+            {   {{ "f", "o", "o", " " }, { " ", "b", "a", "r" }},
+                {{   6,   6,   6,   7 }, {   6,   6,   6,   6 }},
+                TPoint { 4, 0 },
+                TPoint { 0, 0 },
             },
         },
     };
@@ -353,16 +558,19 @@ TEST(TEditor, ShouldDrawTextAndPlaceCursorCorrectly)
         auto text = testCase.input.text;
         auto delta = testCase.input.delta;
         auto *editor = new TEditor(bounds, nullptr, nullptr, nullptr, 256);
-        auto *group = new DrawableTestGroup(bounds);
+        auto *group = new DrawableTestGroup(bounds, testCase.input.events);
         group->setState(sfExposed, true);
         group->getBuffer();
         group->insert(editor);
 
         editor->insertText(text.data(), text.size(), false);
-        for (const auto &event : testCase.input.events)
+        editor->setCurPtr(0, 0);
+        TEvent inputEvent;
+        group->getEvent(inputEvent);
+        while (inputEvent.what != evNothing)
         {
-            TEvent inputEvent = event;
             editor->handleEvent(inputEvent);
+            group->getEvent(inputEvent);
         }
         editor->scrollTo(delta.x, delta.y);
 

test/tvision/tmnuview.test.cpp
@@ -10,6 +10,7 @@
 #include <test.h>
 #include <test_operators.h>
 #include <test_charops.h>
+#include <test_group.h>
 
 enum : ushort
 {
@@ -64,38 +65,6 @@ static std::ostream &operator<<(std::ostream &os, const MenuItemsAndEventQueue &
     os << "Event queue: " << testing::PrintToString(menuItemsAndEventQueue.eventQueue);
     return os;
 }
-
-class TestGroup : public TGroup
-{
-public:
-
-    TestGroup(std::forward_list<TEvent> aEventQueue) :
-        TGroup(TRect()),
-        eventQueue(std::move(aEventQueue))
-    {
-    }
-
-    void getEvent(TEvent &event) override
-    {
-        if (!eventQueue.empty())
-        {
-            event = eventQueue.front();
-            eventQueue.pop_front();
-        }
-        else
-            // Force the menu to exit.
-            event = messageEv(evCommand, cmNoItemChosen);
-    }
-
-    void putEvent(TEvent &event) override
-    {
-        eventQueue.push_front(event);
-    }
-
-private:
-
-    std::forward_list<TEvent> eventQueue;
-};
 
 TEST(TMenuView, ShouldSelectMenuItemOnKeyPress)
 {
@@ -155,7 +124,7 @@ TEST(TMenuView, ShouldSelectMenuItemOnKeyPress)
         auto *menuView = new TMenuView(TRect(), &menu, nullptr);
         menuView->options |= ofPreProcess;
 
-        auto *group = new TestGroup(input.eventQueue);
+        auto *group = new TestGroup(input.eventQueue, messageEv(evCommand, cmNoItemChosen));
         group->insert(menuView);
         menuView->select();
 
@@ -180,7 +149,7 @@ TEST(TMenuView, ShouldNotSelectMenuItemOnKeyPress)
         {   *new TSubMenu("~F~ile", 0) +
               *new TMenuItem("Exit", cmMenuItemChosen, kbNoKey), // No shortcut.
             {   keyDownEv(kbAltF, kbLeftAlt),
-                keyDownEv(kbNoKey, 0x0000), // No text.
+                keyDownEv(kbNoKey), // No text.
             },
         },
     };
@@ -196,7 +165,7 @@ TEST(TMenuView, ShouldNotSelectMenuItemOnKeyPress)
         auto *menuView = new TMenuView(TRect(), &menu, nullptr);
         menuView->options |= ofPreProcess;
 
-        auto *group = new TestGroup(input.eventQueue);
+        auto *group = new TestGroup(input.eventQueue, messageEv(evCommand, cmNoItemChosen));
         group->insert(menuView);
         menuView->select();
 